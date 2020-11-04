319569445952f7aef522b3f8b7b1fd45163d6217
Tania Polishchuk <tania.polischuk1998@gmail.com>
2020-11-04 11:19:50 +0200

    #4 return logo and App div

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,29 @@
 import React from 'react';
 import './App.css';
+import logo from './logo.svg';
 import { RenderRoutes } from './navigation/Router'
 import { ROUTER_CONFIG } from './navigation/routerConfig'
 
 
 function App() {
   return (
+    <div className="App">
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <p>
+        Edit <code>src/App.tsx</code> and save to reload.
+      </p>
+      <a
+        className="App-link"
+        href="https://reactjs.org"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Learn React
+      </a>
+    </header>
     <RenderRoutes routes={ROUTER_CONFIG} />
+  </div>
   );
 }
 
