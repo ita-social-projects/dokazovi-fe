abebc16d843c93e64f820b02177a34856bc68fcd
Gtes <gates1993@gmail.com>
2020-11-14 19:14:39 +0200

    feat: add newest container, pagination logic for mock data

--- src/modules/main/components/MainView.tsx
+++ src/modules/main/components/MainView.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import NewestContainer from './NewestContainer';
 
 export interface IMainViewProps {}
 
 const MainView: React.FC<IMainViewProps> = (props) => {
-  return <>MainView</>;
+  return (
+    <>
+      <NewestContainer />
+    </>
+  );
 };
 
 export default MainView;
