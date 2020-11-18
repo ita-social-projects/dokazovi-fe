12cdaf052af7300b6d1ea8ba285287e08f71a45d
Oleksandr Dubovenko <al.dubovenko@gmail.com>
2020-11-18 12:41:30 +0200

    update Important Container

--- src/modules/main/components/ImportantContainer.tsx
+++ src/modules/main/components/ImportantContainer.tsx
@@ -2,10 +2,10 @@ import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import Carousel from '../../../lib/components/Carousel';
 import { PostCard } from '../../../lib/components/PostCard';
-import { IRootState } from '../../../store/rootReducer';
-import { loadImportant } from '../store/actions';
+import { RootState } from '../../../store/store';
+import { loadImportant } from '../store/mainSlice';
 
-const selectImportant = (state: IRootState) => state.main.important;
+const selectImportant = (state: RootState) => state.main.important;
 
 const ImportantContainer: React.FC = () => {
   const posts = useSelector(selectImportant);
