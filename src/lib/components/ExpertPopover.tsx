79669e3afb094365d5cc22953e63f9420d850193
DelFlumen <delflumen@ukr.net>
2020-11-18 22:49:07 +0200

    feat: ExpertDataCard

--- src/lib/components/ExpertPopover.tsx
+++ src/lib/components/ExpertPopover.tsx
@@ -1,9 +1,6 @@
 import React, { ReactNode, useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Popover from '@material-ui/core/Popover';
-import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
-import { IExpertProps } from './ExpertBlock';
 
 const useStyles = makeStyles((theme) => ({
   typography: {
@@ -22,15 +19,6 @@ export const ExpertPopover: React.FC<{
 }> = (props) => {
   const { children, anchorEl, handlePopoverClose } = props;
   const classes = useStyles();
-  // const [anchorEl, setAnchorEl] = useState<HTMLDivElement | null>(null);
-
-  // const handlePopoverOpen = (event: React.MouseEvent<HTMLDivElement>) => {
-  //   setAnchorEl(event.currentTarget);
-  // };
-
-  // const handleClose = () => {
-  //   setAnchorEl(null);
-  // };
 
   const open = Boolean(anchorEl);
   const id = open ? 'simple-popover' : undefined;
