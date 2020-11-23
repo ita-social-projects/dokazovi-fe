730c97a12b6aaa14d381db78af030da8ed0a34a5
Anton Gluschuk <32397351+AntonGluschuk@users.noreply.github.com>
2020-11-23 13:43:19 +0200

    add jest config files

---
+++ jest.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  roots: ['<rootDir>/src'],
+  testMatch: [
+    '**/__tests__/**/*.+(ts|tsx|js)',
+    '**/?(*.)+(spec|test).+(ts|tsx|js)',
+  ],
+  transform: {
+    '^.+\\.(ts|tsx)$': 'ts-jest',
+    '.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+      '<rootDir>/jest-config/file-mock.js',
+    '.(css|less)$': '<rootDir>/jest-config/style-mock.js',
+  },
+};
