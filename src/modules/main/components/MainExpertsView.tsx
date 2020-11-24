d5cb8f40a4d0efcb671c33cab5c4fa73fd0f3312
Yaroslav Zaychenko <66166679+DelFlumen@users.noreply.github.com>
2020-11-24 12:28:41 +0200

    Issue 61 fix expert popover (#62)
    * fix: ExpertDataCard
    
    * fix: ExpertPopover
    
    * fix: MainExpertsView style
    
    * fix: ExpertDataCard
    
    * fix: ExpertDataCard

--- src/modules/main/components/MainExpertsView.tsx
+++ src/modules/main/components/MainExpertsView.tsx
@@ -1,19 +1,26 @@
 import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { Container, Typography } from '@material-ui/core';
 import { ExpertBlock } from '../../../lib/components/ExpertBlock';
 import { useStyles } from '../styles/MainExpertsView.styles';
 import { fetchExperts } from '../store/mainSlice';
 import { RootStateType } from '../../../store/rootReducer';
 import { ExpertPopover } from '../../../lib/components/ExpertPopover';
 import ExpertDataCard from '../../../lib/components/ExpertDataCard';
+import { IExpert } from '../../../lib/types';
 
 const cardsClasses = Array.from(Array(11).keys()).map((el) => `item_${el}`);
 const selectExperts = (state: RootStateType) => state.main.experts;
 
 export const MainExpertsView: React.FC = () => {
   const [anchorEl, setAnchorEl] = useState<HTMLDivElement | null>(null);
+  const [popoverCard, setPopoverCard] = useState<IExpert | null>(null);
 
-  const handlePopoverOpen = (event: React.MouseEvent<HTMLDivElement>) => {
+  const handlePopoverOpen = (
+    event: React.MouseEvent<HTMLDivElement>,
+    card: IExpert,
+  ) => {
+    setPopoverCard(card);
     setAnchorEl(event.currentTarget);
   };
 
@@ -30,26 +37,28 @@ export const MainExpertsView: React.FC = () => {
   const allExperts = cards.map((card, key) => (
     <>
       <div
-        onMouseEnter={handlePopoverOpen}
+        onMouseEnter={(event) => handlePopoverOpen(event, card)}
         onMouseLeave={handlePopoverClose}
         key={card.phone}
         className={classes[cardsClasses[key]] as string}
       >
         <ExpertBlock expert={card} />
       </div>
-      <ExpertPopover
-        anchorEl={anchorEl}
-        handlePopoverClose={handlePopoverClose}
-      >
-        <ExpertDataCard expert={card} />
-      </ExpertPopover>
     </>
   ));
 
   return (
-    <>
-      <h2>Експерти</h2>
-      <div className={classes.container}> {allExperts}</div>
-    </>
+    <Container>
+      <Typography variant="h4">Експерти</Typography>
+      <div className={classes.container}>
+        {allExperts}
+        <ExpertPopover
+          anchorEl={anchorEl}
+          handlePopoverClose={handlePopoverClose}
+        >
+          {popoverCard && <ExpertDataCard expert={popoverCard} />}
+        </ExpertPopover>
+      </div>
+    </Container>
   );
 };
