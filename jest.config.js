f648aac6e227375a5b2228ec23482da1d5294411
Anton Gluschuk <32397351+AntonGluschuk@users.noreply.github.com>
2020-11-24 13:03:38 +0200

    add identity-obj-proxy

--- jest.config.js
+++ jest.config.js
@@ -8,6 +8,8 @@ module.exports = {
     '^.+\\.(ts|tsx)$': 'ts-jest',
     '.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
       '<rootDir>/jest-config/file-mock.js',
-    '.(css|less)$': '<rootDir>/jest-config/style-mock.js',
+  },
+  moduleNameMapper: {
+    '\\.(css|less)$': 'identity-obj-proxy',
   },
 };
