a88542bec7ec4a32524f015415f5ecf1d8c2b67c
Oleksandr Dubovenko <al.dubovenko@gmail.com>
2020-11-27 15:52:26 +0200

    add case reducers, connect directionSlice to rootReducer

--- src/modules/direction/store/directionSlice.tsx
+++ src/modules/direction/store/directionSlice.tsx
@@ -1,14 +1,35 @@
 /* eslint-disable no-param-reassign */
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 import { MOCK_COURSES } from '../courses/directionCourses.mock';
-import { ICourse } from '../../../lib/types';
+import { ICourse, IExpert, IPost } from '../../../lib/types';
 import type { AppThunkType } from '../../../store/store';
 
+// tmp type
+interface IMeta {
+  totalPosts?: number;
+  limit?: number;
+  currentIndex: number;
+  showMore?: boolean;
+}
+export interface IMaterialsPayload {
+  posts: IPost[];
+  meta: IMeta;
+}
+
 export interface IDirectionState {
+  experts: IExpert[];
+  materials: IMaterialsPayload;
   courses: ICourse[];
 }
 
 const initialState: IDirectionState = {
+  experts: [],
+  materials: {
+    posts: [],
+    meta: {
+      currentIndex: 0,
+    },
+  },
   courses: [],
 };
 
@@ -16,13 +37,23 @@ export const directionSlice = createSlice({
   name: 'direction',
   initialState,
   reducers: {
+    loadExperts: (state, action: PayloadAction<IExpert[]>) => {
+      state.experts = action.payload;
+    },
+    loadMaterials: (state, action: PayloadAction<IMaterialsPayload>) => {
+      state.materials = action.payload;
+    },
     loadCourses: (state, action: PayloadAction<ICourse[]>) => {
       state.courses = action.payload;
     },
   },
 });
 
-export const { loadCourses } = directionSlice.actions;
+export const {
+  loadExperts,
+  loadMaterials,
+  loadCourses,
+} = directionSlice.actions;
 
 export default directionSlice.reducer;
 
