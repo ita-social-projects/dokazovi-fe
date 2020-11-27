94e4fc822db67a887bbb4ee20f091d75b0e0aa8a
Solomaha Darya <solomaha_darya@ukr.net>
2020-11-27 12:02:13 +0200

    add logic to rootReducer

--- src/store/rootReducer.ts
+++ src/store/rootReducer.ts
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
+import { directionReducer } from '../modules/direction/store/directionReducer';
 import mainReducer from '../modules/main/store/mainSlice';
 
 const rootReducer = combineReducers({
   main: mainReducer,
+  direction: directionReducer,
 });
 
 export type RootStateType = ReturnType<typeof rootReducer>;
