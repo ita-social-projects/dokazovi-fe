763b23052a331d7f8ae5124ef043cf68d7414df0
Solomaha Darya <solomaha_darya@ukr.net>
2020-11-27 11:56:21 +0200

    resolve header conflict

--- src/lib/components/Header/Header.tsx
+++ src/lib/components/Header/Header.tsx
@@ -1,14 +1,55 @@
+
 import React from 'react';
 import AccountCircleIcon from '@material-ui/icons/AccountCircle';
 import LocalHospitalIcon from '@material-ui/icons/LocalHospital';
 import { Grid, Link, Toolbar, Typography } from '@material-ui/core';
 import ListItem from '@material-ui/core/ListItem/ListItem';
 import List from '@material-ui/core/List/List';
-import ListItems from './ListItems';
 import { useStyles } from './Header.styles';
+
+interface IHeaderProps {
+  id: string, 
+  label: string,
+  url?: string,
+  }
+
+export const navElems: IHeaderProps [] = [
+  {
+    id: "main",
+    label: "Головна",
+    url: "/"
+  },
+  {
+    id: "covid-19",
+    label: "Covid-19",
+    url: "direction/covid-19",
+ },
+ {
+  id: "directions",
+  label: "Напрямки",
+},
+{
+  id: "experts",
+  label: "Експерти",
+},
+{
+  id: "translates",
+  label: "Переклади",
+},
+{
+  id: "study",
+  label: "Навчання",
+}
+];
+
 
 const Header: React.FC = () => {
   const classes = useStyles();
+
+  const allLinks: any = navElems.map((item) => {
+    return <a key={item.id} href={item.url} className={classes.items}>{item.label}</a>;
+  });
+
   return (
     <div className="header">
       <Grid container>
@@ -36,7 +77,7 @@ const Header: React.FC = () => {
         <Grid item xs={12}>
           <List className={classes.generalNavigation}>
             <ListItem>
-                <ListItems id='' label=''/>
+                {allLinks}
             </ListItem>
           </List>
         </Grid>
