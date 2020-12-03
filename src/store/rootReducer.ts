8d2754331dfbef92b520c5f6aee8ae595748c56a
Solomaha Darya <solomaha_darya@ukr.net>
2020-12-03 15:40:22 +0200

    changes at rootReducer

--- src/store/rootReducer.ts
+++ src/store/rootReducer.ts
@@ -2,10 +2,14 @@ import { combineReducers } from 'redux';
 import directionReducer from '../modules/direction/store/directionSlice';
 import mainReducer from '../modules/main/store/mainSlice';
 
+let directions = ['covid19', 'therapy'];
+
 const rootReducer = combineReducers({
-  main: mainReducer,
-  direction: directionReducer,
+    main: mainReducer,
+    direction: directionReducer,
 });
+
+
 
 export type RootStateType = ReturnType<typeof rootReducer>;
 
