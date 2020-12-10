e5c67dd0f5a945858e868e38ef42d70136bd1942
DelFlumen <delflumen@ukr.net>
2020-12-10 17:52:42 +0200

    expertsSlice draft

--- src/modules/experts/store/expertsSlice.ts
+++ src/modules/experts/store/expertsSlice.ts
@@ -1,9 +1,10 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 import { IPost, LoadingStatusEnum } from '../../../lib/types';
 import { IExpertPayload } from '../../main/store/mainSlice';
 
 export interface IExpertsState {
   experts: IExpertPayload;
-  materials: IMaterialsPayload;
+  materials: [];
 }
 
 interface IMaterialsPayload {
@@ -23,3 +24,29 @@ const initialState: IExpertsState = {
   },
   materials: [],
 };
+
+export const expertsSlice = createSlice({
+    name: 'experts',
+    initialState,
+    reducers: {
+        loadExperts: (state, action: PayloadAction<IExpertPayload>) => {
+            state.experts = action.payload;
+        },
+        setExpertsRegion: (state, action: PayloadAction<number | number[]>) => {
+            state.experts.meta.regionFilter = action.payload;
+        },
+        setExpertsDirection: (state, action: PayloadAction<number | number[]>) => {
+            state.experts.meta.directionFilter = action.payload;
+        }
+
+    }
+});
+
+export const {
+    loadExperts,
+    setExpertsRegion,
+    setExpertsDirection,    
+  } = expertsSlice.actions;
+  
+  export const expertsReducer = expertsSlice.reducer;
+ 
