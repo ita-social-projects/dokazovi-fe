037339d884e619ba5973a4b5aaecd366d2d24575
DelFlumen <delflumen@ukr.net>
2020-12-10 13:44:17 +0200

    FilterForm draft

---
+++ src/lib/components/FilterForm.tsx
@@ -0,0 +1,77 @@
+import React, { useCallback } from 'react';
+import { withStyles } from '@material-ui/core/styles';
+import { green } from '@material-ui/core/colors';
+import FormGroup from '@material-ui/core/FormGroup';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+// import debounce from 'lodash.debounce';
+import _ from 'lodash';
+import CheckBoxOutlineBlankIcon from '@material-ui/icons/CheckBoxOutlineBlank';
+import CheckBoxIcon from '@material-ui/icons/CheckBox';
+import Favorite from '@material-ui/icons/Favorite';
+import FavoriteBorder from '@material-ui/icons/FavoriteBorder';
+import { StaticRouter } from 'react-router-dom';
+
+// const GreenCheckbox = withStyles({
+//   root: {
+//     color: green[400],
+//     '&$checked': {
+//       color: green[600],
+//     },
+//   },
+//   checked: {},
+// })((props) => <Checkbox color="default" {...props} />);
+
+export type CheckedType = {
+  [key: string]: boolean;
+};
+
+export const FilterForm: React.FC = () => {
+  // const {register, handleSubmit} = useForm();
+  const [state, setState] = React.useState<CheckedType>({
+    Kyiv: false,
+    Lviv: false,
+    Kharkiv: false,
+  });
+
+  const regions = ['Kyiv', 'Lviv', 'Kharkiv'];
+
+  // const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+  //   const checkboxName = event?.target?.name;
+  //   setState({ ...state, [checkboxName]: event.target.checked });
+  //   console.log(state);
+  // };
+
+  const stateLog = () => {
+    console.log(state);
+  };
+
+  const handler = useCallback(_.debounce(stateLog, 2000), []);
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const checkboxName = event?.target?.name;
+    setState({ ...state, [checkboxName]: event.target.checked });
+    // console.log(state);
+    handler();
+  };
+
+  return (
+    <>
+      <FormGroup row>
+        {regions.map((elem, index) => (
+          <FormControlLabel
+            control={
+              <Checkbox
+                checked={state ? state[elem] : false}
+                onChange={handleChange}
+                name={elem}
+              />
+            }
+            label={elem}
+            key={elem}
+          />
+        ))}
+      </FormGroup>
+    </>
+  );
+};
--- src/modules/main/components/MainView.tsx
+++ src/modules/main/components/MainView.tsx
@@ -6,6 +6,7 @@ import ImportantContainer from './ImportantContainer';
 import { ExpertsView } from '../../../lib/components/ExpertsView';
 import { RootStateType } from '../../../store/rootReducer';
 import { fetchExperts } from '../store/mainSlice';
+import { FilterForm } from '../../../lib/components/FilterForm';
 
 export interface IMainViewProps {}
 
@@ -25,6 +26,7 @@ const MainView: React.FC<IMainViewProps> = () => {
 
   return (
     <>
+      <FilterForm />
       <Container>
         <Grid container spacing={2} direction="row" alignItems="center">
           <Grid item xs={12}>
