8c3b9168b4c5c32acce7573271a38a582134e0f1
DelFlumen <delflumen@ukr.net>
2020-12-10 18:15:32 +0200

    expertsSlice draft

--- src/modules/experts/store/expertsSlice.ts
+++ src/modules/experts/store/expertsSlice.ts
@@ -1,3 +1,4 @@
+/* eslint-disable no-param-reassign */
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 import { IPost, LoadingStatusEnum } from '../../../lib/types';
 import { IExpertPayload } from '../../main/store/mainSlice';
