71a589dc92adc5d51f76643906f7faf50869d5e2
DelFlumen <delflumen@ukr.net>
2020-12-17 11:14:37 +0200

    fix router-config

--- src/navigation/router-config.tsx
+++ src/navigation/router-config.tsx
@@ -3,7 +3,9 @@ import { RenderRoutes } from './Router';
 import { IRouterConfig } from './types';
 
 const MainView = lazy(() => import('../modules/main/components/MainView'));
-const DirectionsList = lazy(() => import('../modules/direction/components/DirectionsList'));
+const DirectionsList = lazy(
+  () => import('../modules/direction/components/DirectionsList'),
+);
 const DirectionView = lazy(
   () => import('../modules/direction/components/DirectionView'),
 );
@@ -12,42 +14,41 @@ const ExpertsView = lazy(
 );
 
 const ROUTER_CONFIG: IRouterConfig[] = [
-    { path: '/', key: 'ROOT', exact: true, component: MainView },
-    {
+  { path: '/', key: 'ROOT', exact: true, component: MainView },
+  {
+    path: '/direction',
+    key: 'DIRECTION',
+    exact: false,
+    component: RenderRoutes,
+    routes: [
+      {
         path: '/direction',
         key: 'DIRECTION',
         exact: false,
         component: RenderRoutes,
         routes: [
-            {
-                path: '/direction',
-                key: 'DIRECTION',
-                exact: false,
-                component: RenderRoutes,
-                routes: [
-                    {
-                        path: '/direction',
-                        key: 'DIRECTION_ROOT',
-                        exact: true,
-                        component: DirectionsList,
-                    },
-                    {
-                        path: '/direction/:id',
-                        key: 'DIRECTION_COMPONENT',
-                        exact: true,
-                        component: DirectionView,
-                    }
-                ],
-            },
-            {
-                path: '/experts',
-                key: 'EXPERTS',
-                exact: true,
-                component: ExpertsView,
-            },
+          {
+            path: '/direction',
+            key: 'DIRECTION_ROOT',
+            exact: true,
+            component: DirectionsList,
+          },
+          {
+            path: '/direction/:id',
+            key: 'DIRECTION_COMPONENT',
+            exact: true,
+            component: DirectionView,
+          },
+        ],
+      },
     ],
-    },
+  },
+  {
+    path: '/experts',
+    key: 'EXPERTS',
+    exact: true,
+    component: ExpertsView,
+  },
 ];
-
 
 export default ROUTER_CONFIG;
