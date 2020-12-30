878bede22753f66345d025e563a5660d8aa383f7
DelFlumen <delflumen@ukr.net>
2020-12-30 19:06:31 +0200

    filter accordion

--- src/lib/components/FilterFormCopy.tsx
+++ src/lib/components/FilterFormCopy.tsx
@@ -2,6 +2,10 @@ import React, { useCallback, useEffect } from 'react';
 import FormGroup from '@material-ui/core/FormGroup';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Checkbox from '@material-ui/core/Checkbox';
+import Accordion from '@material-ui/core/Accordion';
+import AccordionSummary from '@material-ui/core/AccordionSummary';
+import AccordionDetails from '@material-ui/core/AccordionDetails';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import _ from 'lodash';
 import { useDispatch, useSelector } from 'react-redux';
 import { Grid, Typography } from '@material-ui/core';
@@ -31,8 +35,6 @@ export interface IFilterFormProps {
 export const FilterForm: React.FC<IFilterFormProps> = (props) => {
   const { setExpertsListFilter, expertsProperties, filterType } = props;
   const dispatch = useDispatch();
-  //   (state: RootStateType) => state.properties?.regions,
-  // );
 
   const initLocalState = expertsProperties.reduce(
     (acc: ICheckboxes, next: ExpertPropertiesType) => {
@@ -68,6 +70,12 @@ export const FilterForm: React.FC<IFilterFormProps> = (props) => {
     [],
   );
 
+  console.log(expertsProperties);
+
+  // const checkedExpertsProperties = Object.values(expertsProperties).filter(value => checked[entrie[0]] && )
+
+  // console.log(checkedExpertsProperties);
+
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const checkedTypes = {
       ...checked,
@@ -90,51 +98,70 @@ export const FilterForm: React.FC<IFilterFormProps> = (props) => {
 
   return (
     <>
-      <Grid container style={{ paddingLeft: '30px', marginTop: '20px' }}>
-        <Grid item xs={2} style={{ marginRight: '-30px' }}>
-          <Typography variant="h5">{filterTitle}</Typography>
-        </Grid>
-        <Grid item xs={10}>
-          <FormControlLabel
-            style={{ width: '100%' }}
-            control={
-              <Checkbox
-                id="All"
-                checked={allChecked}
-                onChange={handleChangeAll}
-                name="All"
-              />
-            }
-            label="Всі"
-            key="All"
-          />
-        </Grid>
-        <Grid item xs={2} style={{ marginRight: '-30px' }} />
-        <FormGroup
-          style={{
-            height: '450px',
-            display: 'flex',
-            flexDirection: 'column',
-            flexWrap: 'wrap',
-          }}
-        >
-          {expertsProperties.map((type) => (
-            <FormControlLabel
-              style={{ width: '100%' }}
-              control={
-                <Checkbox
-                  id={type.id.toString()}
-                  checked={checked[type.id.toString()]}
-                  onChange={handleChange}
-                  name={type.name}
+      <div style={{ width: '100%', paddingLeft: '30px', marginTop: '20px' }}>
+        <Accordion>
+          <AccordionSummary
+            expandIcon={<ExpandMoreIcon />}
+            aria-controls="panel1bh-content"
+            id="panel1bh-header"
+          >
+            <Grid container>
+              <Grid item xs={2} style={{ marginRight: '-30px' }}>
+                <Typography variant="h5">{filterTitle}</Typography>
+              </Grid>
+              <Grid item xs={10}>
+                {checked && <div>Обране</div>}
+              </Grid>
+            </Grid>
+          </AccordionSummary>
+
+          <AccordionDetails>
+            <Grid container>
+              <Grid item xs={2} style={{ marginRight: '-30px' }} />
+              <Grid item xs={10}>
+                <FormControlLabel
+                  style={{ width: '100%' }}
+                  control={
+                    <Checkbox
+                      id="All"
+                      checked={allChecked}
+                      onChange={handleChangeAll}
+                      name="All"
+                    />
+                  }
+                  label="Всі"
+                  key="All"
                 />
-              }
-              label={type.name}
-              key={type.name}
-            />
-          ))}
-        </FormGroup>{' '}
-      </Grid>
+              </Grid>
+              <Grid item xs={2} style={{ marginRight: '-30px' }} />
+              <FormGroup
+                style={{
+                  height: '450px',
+                  display: 'flex',
+                  flexDirection: 'column',
+                  flexWrap: 'wrap',
+                }}
+              >
+                {expertsProperties.map((type) => (
+                  <FormControlLabel
+                    style={{ width: '100%' }}
+                    control={
+                      <Checkbox
+                        id={type.id.toString()}
+                        checked={checked[type.id.toString()]}
+                        onChange={handleChange}
+                        name={type.name}
+                      />
+                    }
+                    label={type.name}
+                    key={type.name}
+                  />
+                ))}
+              </FormGroup>{' '}
+            </Grid>
+          </AccordionDetails>
+        </Accordion>
+      </div>
     </>
   );
 };
