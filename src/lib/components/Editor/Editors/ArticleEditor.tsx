9cb492f6869452ef4e5987fcf6ab24c1ae3eb9d3
Gtes <gates1993@gmail.com>
2021-01-12 20:57:36 +0200

    add preview container to the editors

--- src/lib/components/Editor/Editors/ArticleEditor.tsx
+++ src/lib/components/Editor/Editors/ArticleEditor.tsx
@@ -1,22 +1,34 @@
+import { Container } from '@material-ui/core';
 import React, { useEffect, useRef, useState } from 'react';
 import ReactQuill, { Quill } from 'react-quill';
+import BorderBottom from '../../Border';
+import ContentPreviewContainer from '../ContentPreviewContainer';
 import GeneralEditor from '../GeneralEditor';
 import ArticleEditorToolbar from './ArticleEditorToolbar';
 
 const ArticleEditor: React.FC = () => {
   const [editor, setEditor] = useState<Quill>();
   const articleEditor = useRef<ReactQuill | null>(null);
+  const [editorContent, setEditorContent] = useState<string>('');
 
   useEffect(() => {
     if (articleEditor.current) setEditor(articleEditor.current.getEditor());
   }, []);
 
+  editor?.on('text-change', () => {
+    setEditorContent(editor.getText());
+  });
+
   return (
     <>
-      <GeneralEditor
-        toolbar={<ArticleEditorToolbar editor={editor} />}
-        ref={articleEditor}
-      />
+      <Container>
+        <GeneralEditor
+          toolbar={<ArticleEditorToolbar editor={editor} />}
+          ref={articleEditor}
+        />
+        <BorderBottom />
+        <ContentPreviewContainer previewText={editorContent} />
+      </Container>
     </>
   );
 };
--- src/lib/components/Editor/Editors/NoteEditor.tsx
+++ src/lib/components/Editor/Editors/NoteEditor.tsx
@@ -1,22 +1,34 @@
+import { Container } from '@material-ui/core';
 import React, { useEffect, useRef, useState } from 'react';
 import ReactQuill, { Quill } from 'react-quill';
+import BorderBottom from '../../Border';
+import ContentPreviewContainer from '../ContentPreviewContainer';
 import GeneralEditor from '../GeneralEditor';
 import NoteEditorToolbar from './NoteEditorToolbar';
 
 const NoteEditor: React.FC = () => {
   const [editor, setEditor] = useState<Quill>();
   const noteEditor = useRef<ReactQuill | null>(null);
+  const [editorContent, setEditorContent] = useState<string>('');
 
   useEffect(() => {
     if (noteEditor.current) setEditor(noteEditor.current.getEditor());
   }, []);
 
+  editor?.on('text-change', () => {
+    setEditorContent(editor.getText());
+  });
+
   return (
     <>
-      <GeneralEditor
-        toolbar={<NoteEditorToolbar editor={editor} />}
-        ref={noteEditor}
-      />
+      <Container>
+        <GeneralEditor
+          toolbar={<NoteEditorToolbar editor={editor} />}
+          ref={noteEditor}
+        />
+        <BorderBottom />
+        <ContentPreviewContainer previewText={editorContent} />
+      </Container>
     </>
   );
 };
