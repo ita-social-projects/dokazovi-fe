d451857558d74eee54d6b029e83c85d85fba8391
Gtes <gates1993@gmail.com>
2021-01-14 18:09:41 +0200

    delete courses

--- src/lib/components/Header/Header.tsx
+++ src/lib/components/Header/Header.tsx
@@ -39,10 +39,6 @@ export const navElems: IHeaderProps[] = [
     id: 'translates',
     label: 'Переклади',
   },
-  {
-    id: 'study',
-    label: 'Навчання',
-  },
 ];
 
 const Header: React.FC = () => {
