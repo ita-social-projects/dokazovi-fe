2a2ef5ac23802769db2532695c6ac4274de73b9a
Anna Minasian <32515945+AnnaMinasian@users.noreply.github.com>
2021-02-18 21:10:07 +0200

    Added redirect to important posts (post, expert) #249

--- src/lib/components/PostCard.tsx
+++ src/lib/components/PostCard.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import Card from '@material-ui/core/Card';
 import Typography from '@material-ui/core/Typography';
 import CardMedia from '@material-ui/core/CardMedia';
@@ -13,11 +14,21 @@ export interface IPostCardProps {
 
 export const PostCard: React.FC<IPostCardProps> = (props) => {
   const classes = useStyles();
+  const history = useHistory();
   const { post } = props;
+  const author = post.author?.id || '';
+
+  const goPostPage = () => {
+    history.push(`/posts/${post.id}`);
+  };
+
+  const goExpertPage = () => {
+    history.push(`/experts/${author}`);
+  };
 
   return (
     <Card className={classes.root}>
-      <Box className={classes.leftPart}>
+      <Box className={classes.leftPart} onClick={goExpertPage}>
         <CardMedia
           className={classes.photo}
           image={post.author?.avatar}
@@ -36,9 +47,9 @@ export const PostCard: React.FC<IPostCardProps> = (props) => {
           {post.author?.mainInstitution?.name}
         </Typography>
       </Box>
-      <Box className={classes.rightPart}>
+      <Box className={classes.rightPart} onClick={goPostPage}>
         <Box className={classes.chipRoot}>
-          <Chip label={post.postType?.name} size="small" />
+          <Chip label={post.postType?.name} size="small" clickable />
           <Box display="flex" flexDirection="column">
             {post.directions?.map((d) => {
               return <PostDirectionLink direction={d} key={d.id} />;
