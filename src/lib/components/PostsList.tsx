765f570c78c06aa4f47ee72b536e1a898e55f292
Vadym Ohyr <dusty3ntity@gmail.com>
2021-02-22 23:39:00 +0200

    Implemented masonry grid for posts list

--- src/lib/components/PostsList.tsx
+++ src/lib/components/PostsList.tsx
@@ -1,5 +1,7 @@
-import { Grid } from '@material-ui/core';
+import { Box } from '@material-ui/core';
 import React from 'react';
+import Masonry from 'react-masonry-css';
+import { useStyles, masonryBreakpoints } from '../styles/PostsList.styles';
 import { IPost } from '../types';
 import PostPreviewCard from './PostPreview/PostPreviewCard';
 
@@ -8,16 +10,21 @@ export interface IPostsListProps {
 }
 
 const PostsList: React.FC<IPostsListProps> = (props) => {
+  const classes = useStyles();
   const { postsList } = props;
 
   return (
-    <>
+    <Masonry
+      breakpointCols={masonryBreakpoints}
+      className={classes.masonryGrid}
+      columnClassName={classes.masonryColumn}
+    >
       {postsList.map((post) => (
-        <Grid item xs={12} lg={4} md={6} key={post.id}>
+        <Box key={post.id} className={classes.masonryItem}>
           <PostPreviewCard data={post} />
-        </Grid>
+        </Box>
       ))}
-    </>
+    </Masonry>
   );
 };
 
---
+++ src/lib/styles/PostsList.styles.ts
@@ -0,0 +1,24 @@
+import { createStyles, makeStyles, Theme } from '@material-ui/core';
+import { MAIN_THEME } from '../theme/theme';
+
+export const useStyles = makeStyles(
+  (theme: Theme) =>
+    createStyles({
+      masonryGrid: {
+        display: 'flex',
+      },
+      masonryColumn: {
+        margin: `0 ${theme.spacing()}px`,
+      },
+      masonryItem: {
+        marginBottom: theme.spacing(2),
+      },
+    }),
+  { name: 'PostsList' },
+);
+
+export const masonryBreakpoints = {
+  default: 3,
+  [MAIN_THEME.breakpoints.values.md]: 2,
+  [MAIN_THEME.breakpoints.values.sm]: 1,
+};
