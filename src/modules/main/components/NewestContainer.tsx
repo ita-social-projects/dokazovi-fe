72a8effc7ce9b7909bd999fbd92ca260ed7c534c
Anna Minasian <32515945+AnnaMinasian@users.noreply.github.com>
2021-03-23 10:11:52 +0200

    Deleted title and "More materials" button on the Newest section (Main Page)

--- src/modules/main/components/NewestContainer.tsx
+++ src/modules/main/components/NewestContainer.tsx
@@ -1,27 +1,20 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { Grid, Typography } from '@material-ui/core';
 import { RootStateType } from '../../../store/rootReducer';
-import { fetchNewestPosts, fetchInitialNewestPosts } from '../store/mainSlice';
+import { fetchInitialNewestPosts } from '../store/mainSlice';
 import { useStyles } from '../styles/NewestContainer.style';
 import PostsList from '../../../lib/components/Posts/PostsList';
 import { LoadingStatusEnum } from '../../../lib/types';
-import useEffectExceptOnMount from '../../../lib/hooks/useEffectExceptOnMount';
 import { selectPostsByIds } from '../../../store/selectors';
-import LoadMorePostsButton from '../../../lib/components/LoadMorePostsButton';
 import LoadingContainer from '../../../lib/components/Loading/LoadingContainer';
 
 const NewestContainer: React.FC = () => {
   const classes = useStyles();
   const dispatch = useDispatch();
 
-  const loadMore = () => {
-    dispatch(fetchNewestPosts());
-  };
-
   const {
     newestPostIds,
-    meta: { isLastPage, loading, currentPage },
+    meta: { loading },
   } = useSelector((state: RootStateType) => state.main.newest);
   const newestPosts = selectPostsByIds(newestPostIds);
 
@@ -30,29 +23,14 @@ const NewestContainer: React.FC = () => {
     setNewestInitial();
   }, []);
 
-  const gridRef = useRef<HTMLDivElement>(null);
-  useEffectExceptOnMount(() => {
-    if (currentPage > 1) {
-      gridRef.current?.scrollIntoView({ behavior: 'smooth' });
-    }
-  }, [currentPage]);
-
   return (
     <div className={classes.container}>
-      {loading === LoadingStatusEnum.pending && currentPage === 0 ? (
+      {loading === LoadingStatusEnum.pending ? (
         <LoadingContainer loading={loading} expand />
       ) : (
         <>
-          <Typography variant="h4">Найновіше</Typography>
           <PostsList postsList={newestPosts} />
           <LoadingContainer loading={loading} />
-          <Grid container direction="column" alignItems="center" ref={gridRef}>
-            <LoadMorePostsButton
-              clicked={loadMore}
-              isLastPage={isLastPage}
-              loading={loading}
-            />
-          </Grid>
         </>
       )}
     </div>
