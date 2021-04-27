c62f67c8b7614186fe4e9aa7bb69e4eed5bd6084
Andrii Kachkarov <andrii.kachkarov@gmail.com>
2021-04-27 18:43:28 +0300

    add ExpertOpinionPostPreviewCard component and styles

---
+++ src/lib/components/Posts/Cards/ExpertOpinionPostPreviewCard/ExpertOpinionPostPreviewCard.styles.ts
@@ -0,0 +1,72 @@
+import { makeStyles, Theme } from '@material-ui/core';
+
+export const useStyles = makeStyles(
+  (theme: Theme) => ({
+    root: {
+      position: 'relative',
+      borderRadius: 0,
+      width: '315px',
+    },
+    avatar: {
+      height: 95,
+      width: 95,
+      padding: theme.spacing(3),
+      paddingLeft: theme.spacing(3),
+    },
+    authorFullName: {
+      fontFamily: 'Raleway',
+      fontStyle: 'normal',
+      fontWeight: 'bold',
+      fontSize: '18px',
+      lineHeight: '21px',
+      marginBottom: theme.spacing(-1),
+    },
+    header: {
+      display: 'flex',
+      paddingLeft: theme.spacing(5),
+      paddingRight: theme.spacing(3),
+    },
+    body: {
+      display: 'flex',
+      flexDirection: 'column',
+      flexGrow: 1,
+      justifyContent: 'space-between',
+      paddingLeft: theme.spacing(5),
+      paddingRight: theme.spacing(6),
+    },
+    textHeader: {
+      wordWrap: 'break-word',
+      fontSize: '24px',
+      lineHeight: '28px',
+      marginBottom: theme.spacing(2.4),
+    },
+    textBody: {
+      wordWrap: 'break-word',
+      marginBottom: theme.spacing(5),
+    },
+    postType: {
+      color: '#60B3CD',
+      fontFamily: 'Raleway',
+      fontStyle: 'normal',
+      fontWeight: 600,
+      fontSize: '10px',
+      lineHeight: '17px',
+      letterSpacing: '0.12em',
+      textTransform: 'uppercase',
+    },
+    content: {
+      padding: 0,
+    },
+    eyeIcon: {
+      fill: theme.palette.text.secondary,
+    },
+    viewsCount: {
+      width: '23px',
+      height: '13px',
+      marginLeft: theme.spacing(2),
+    },
+  }),
+  {
+    name: 'PostPreviewCard',
+  },
+);
---
+++ src/lib/components/Posts/Cards/ExpertOpinionPostPreviewCard/ExpertOpinionPostPreviewCard.tsx
@@ -0,0 +1,131 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Card from '@material-ui/core/Card';
+import Box from '@material-ui/core/Box';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Typography from '@material-ui/core/Typography';
+import { IPost } from '../../../../types';
+import { useStyles } from './ExpertOpinionPostPreviewCard.styles';
+import { formatDate } from '../../../../utilities/formateDate';
+
+export interface IPostPreviewCardProps {
+  post: IPost;
+  shouldNotUseLink?: boolean;
+}
+
+const PostPreviewCardWithoutMemo: React.FC<IPostPreviewCardProps> = ({
+  post,
+  shouldNotUseLink,
+}) => {
+  const classes = useStyles();
+  const expertLink = `/experts/${post.author.id}`;
+  const postLink = `/posts/${post.id}`;
+  const authorFullName = `${post.author.firstName} ${post.author.lastName}`;
+  const authorMainInstitution = post.author.mainInstitution
+    ? `${post.author.mainInstitution.city.name}, ${post.author.mainInstitution.name}`
+    : '';
+
+  const expertBody = (
+    <Box
+      className={classes.header}
+      flexDirection="column"
+      flexWrap="no-wrap"
+      justifyContent="space-between"
+      mb={6}
+    >
+      <Box
+        display="flex"
+        flexDirection="row"
+        flexWrap="no-wrap"
+        justifyContent="space-between"
+        mb={4}
+      >
+        <Box
+          display="flex"
+          flexDirection="column"
+          justifyContent="space-between"
+          pt={4}
+        >
+          <Typography
+            className={classes.postType}
+            variant="overline"
+            component="span"
+          >
+            {post.type.name}
+          </Typography>
+          <Typography
+            className={classes.authorFullName}
+            variant="h5"
+            component="span"
+          >
+            {authorFullName}
+          </Typography>
+        </Box>
+        <CardMedia
+          className={classes.avatar}
+          image={post.author.avatar} // paste default avatar if not present
+          title={authorFullName}
+          component="div"
+        />
+      </Box>
+      <Typography
+        color="textSecondary"
+        variant="subtitle2"
+        component="span"
+        gutterBottom
+      >
+        {authorMainInstitution}
+      </Typography>
+    </Box>
+  );
+
+  const postBody = (
+    <Box className={classes.body}>
+      <CardContent className={classes.content}>
+        <Typography
+          gutterBottom
+          variant="h4"
+          component="h3"
+          className={classes.textHeader}
+        >
+          {post.title}
+        </Typography>
+        <Typography
+          gutterBottom
+          variant="body2"
+          color="textPrimary"
+          component="p"
+          className={classes.textBody}
+        >
+          {post.preview}
+        </Typography>
+      </CardContent>
+      <Box
+        display="flex"
+        justifyContent="space-between"
+        alignItems="center"
+        mb={6}
+      >
+        <Typography variant="caption" color="textSecondary">
+          {formatDate(post.createdAt)}
+        </Typography>
+      </Box>
+    </Box>
+  );
+
+  return (
+    <Card className={classes.root}>
+      {shouldNotUseLink ? (
+        expertBody
+      ) : (
+        <Link to={expertLink}>{expertBody}</Link>
+      )}
+      {shouldNotUseLink ? postBody : <Link to={postLink}>{postBody}</Link>}
+    </Card>
+  );
+};
+
+export const ExpertOpinionPostPreviewCard = React.memo(
+  PostPreviewCardWithoutMemo,
+);
