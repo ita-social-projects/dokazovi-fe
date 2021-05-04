d4807f9987b126a10e48ea7f4ebbde74e77dfcdd
Andrii Kachkarov <andrii.kachkarov@gmail.com>
2021-05-04 20:38:47 +0300

    add VideoPostPreviewCard component and styles

---
+++ src/old/lib/components/Posts/Cards/VideoPostPreviewCard/VideoPostPreviewCard.tsx
@@ -0,0 +1,65 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Card from '@material-ui/core/Card';
+import Box from '@material-ui/core/Box';
+import Typography from '@material-ui/core/Typography';
+import { useStyles } from './VideoPostPreviewCard.styles';
+import { formatDate } from '../../../../utilities/formatDate';
+import background from '../../../../images/mock_img_media_bg.png';
+import { IPostPreviewCardProps } from '../types';
+
+export const VideoPostPreviewCard: React.FC<IPostPreviewCardProps> = ({
+  post,
+  shouldNotUseLink,
+}) => {
+  const classes = useStyles();
+  const postLink = `/posts/${post.id}`;
+
+  const bgImageURL = post.previewImageUrl ? post.previewImageUrl : background;
+
+  const card = (
+    <>
+      <Box
+        className={classes.header}
+        flexDirection="column"
+        flexWrap="no-wrap"
+        justifyContent="space-between"
+        style={{
+          backgroundImage: `url(${bgImageURL})`,
+        }}
+      >
+        <Box className={classes.play} />
+      </Box>
+      <Box className={classes.body}>
+        <Typography
+          gutterBottom
+          variant="h4"
+          component="h3"
+          className={classes.textHeader}
+        >
+          {post.title}
+        </Typography>
+        <Typography
+          gutterBottom
+          variant="body2"
+          color="textPrimary"
+          component="p"
+          className={classes.textBody}
+        >
+          {post.preview}
+        </Typography>
+        <Box mb={6}>
+          <Typography variant="caption" color="textSecondary">
+            {formatDate(post.createdAt)}
+          </Typography>
+        </Box>
+      </Box>
+    </>
+  );
+
+  return (
+    <Card className={classes.root}>
+      {shouldNotUseLink ? card : <Link to={postLink}>{card}</Link>}
+    </Card>
+  );
+};
