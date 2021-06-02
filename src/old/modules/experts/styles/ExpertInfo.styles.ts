9fe4cdde6dc01965cbc4272d90b88957396523b9
Denys <d.viershynin@gmail.com>
2021-06-02 15:18:01 +0300

    redundant styles removed

--- src/old/modules/experts/styles/ExpertInfo.styles.ts
+++ src/old/modules/experts/styles/ExpertInfo.styles.ts
@@ -17,48 +17,8 @@ export const useStyles = makeStyles(() => ({
     width: '280px',
     marginBottom: '80px',
   },
-  contacts: {
-    fontSize: '14px',
-    lineHeight: '14px',
-  },
-  links: {
-    color: '#2051FF',
-  },
   accordionWrapper: {
     width: '280px',
     marginBottom: '50px',
-  },
-  accordion: {
-    width: '280px',
-    backgroundColor: '#E5E5E5',
-  },
-  accordionSummary: {
-    width: '280px',
-    padding: 0,
-  },
-  showContacts: {
-    fontSize: '18px',
-    lineHeight: '18px',
-    fontWeight: 700,
-    fontFamily: 'Raleway',
-    width: '260px',
-  },
-  expandIcon: {
-    width: '20px',
-    height: '20px',
-    fontSize: '36px',
-    lineHeight: '10px',
-    fontWeight: 700,
-    margin: '0px',
-    padding: '0px',
-  },
-  contactIcons: {
-    margin: '0 7px 0 -10px',
-  },
-  headingsDivider: {
-    width: '280px',
-    height: '4px',
-    background: '#000000',
-    margin: '2px 0 0 0',
   },
 }));
