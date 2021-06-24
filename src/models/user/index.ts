54beb7a8f1b986dbdff55f5a7007299f9c092c0b
Petro Khanenko <p.khanenko@gmail.com>
2021-06-24 11:02:56 +0300

    add user loading selector

--- src/models/user/index.ts
+++ src/models/user/index.ts
@@ -1,2 +1,7 @@
 export { getUserAsyncAction } from './asyncActions';
 export { signOutAction, signInReducer } from './reducers';
+export {
+  selectCurrentUserLoading,
+  selectCurrentUserError,
+  selectCurrentUser,
+} from './selectors';
