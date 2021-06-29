b0bede3d8706028ee1097eb5d5f375f01a827973
petro-khanenko <75987847+petro-khanenko@users.noreply.github.com>
2021-06-29 14:05:30 +0300

    Redesign login modal (#435)
    * add user loading selector
    
    * add button disable
    
    * with old variant
    
    * final congratulation added
    
    * final congratulation added
    
    * final congratulation added
    
    Co-authored-by: Petro Khanenko <p.khanenko@gmail.com>

--- src/old/lib/components/Users/LoginModal.styles.ts
+++ src/old/lib/components/Users/LoginModal.styles.ts
@@ -6,22 +6,6 @@ export const useStyles = makeStyles((theme: Theme) => ({
     minWidth: 0,
     padding: 0,
   },
-  submitButton: {
-    padding: '16px 44px',
-    borderRadius: 50,
-    marginRight: '0px',
-    alignSelf: 'flex-end',
-    backgroundColor: '#FF5C00',
-    '& .MuiButton-label': {
-      color: theme.palette.common.white,
-      fontWeight: 700,
-      fontSize: '18px',
-      fontFamily: 'Raleway',
-    },
-    '&:hover': {
-      backgroundColor: '#ffaa00',
-    },
-  },
   icon: {
     width: theme.spacing(11),
     height: theme.spacing(11),
@@ -30,11 +14,14 @@ export const useStyles = makeStyles((theme: Theme) => ({
     color: theme.palette.common.white,
     marginLeft: theme.spacing(4),
   },
-  dialogTitle: {
+  dialogContainer: {
+    position: 'relative',
+  },
+  dialogTitleContainer: {
     height: 60,
     backgroundColor: theme.palette.common.black,
   },
-  titleContainer: {
+  dialogTitleBlock: {
     display: 'flex',
     alignItems: 'center',
     justifyContent: 'space-between',
@@ -48,25 +35,43 @@ export const useStyles = makeStyles((theme: Theme) => ({
     lineHeight: 22,
     wordSpacing: 4,
   },
+  closeIconButton: {
+    width: 50,
+    height: 50,
+    position: 'absolute',
+    right: 7,
+    top: 6,
+  },
+  closeIcon: {
+    color: '#A7A7A7',
+    fontSize: 50,
+  },
   form: {
     width: '432px',
     height: 'fit-content',
-    padding: '45px 15px 35px 15px',
+    padding: '20px 15px 35px 15px',
     position: 'relative',
   },
-  emailInput: {
-    paddingBottom: 20,
-  },
-  bottomContainer: {
-    display: 'flex',
-    justifyContent: 'space-between',
-    margin: '30px 0 15px 0',
-    '& .MuiTypography-body1': {
+  textInput: {
+    marginTop: 35,
+    '& .MuiInputBase-input': {
       fontFamily: 'Raleway',
-      fontWeight: 500,
-      fontSize: 14,
-      lineHeight: '18px',
+      fontWeight: 600,
+      fontSize: 18,
+      lineHeight: 22,
     },
+    '& .MuiFormHelperText-root': {
+      fontFamily: 'Raleway',
+      fontSize: 13,
+      paddingTop: 3,
+    },
+  },
+  visibilityIconButton: {
+    padding: 0,
+  },
+  visibilityIcon: {
+    fontSize: 34,
+    color: 'black',
   },
   error: {
     fontFamily: 'Raleway',
@@ -76,26 +81,93 @@ export const useStyles = makeStyles((theme: Theme) => ({
     color: '#FF0707',
     marginTop: 20,
   },
-  doneIcon: {
-    position: 'absolute',
-    left: 12,
-    bottom: 63,
-    color: 'black',
-    fontSize: 30,
+  bottomContainer: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    margin: '40px 0 10px 0',
+    '& .MuiTypography-body1': {
+      fontFamily: 'Raleway',
+      fontWeight: 500,
+      fontSize: 14,
+      lineHeight: '18px',
+    },
   },
-  closeIconButton: {
-    width: 40,
-    height: 40,
+  uncheckedIcon: {
+    width: 20,
+    height: 20,
+    borderRadius: 3,
+    border: 'solid 2px #CCCCCC',
   },
-  closeIcon: {
-    color: '#A7A7A7',
-    fontSize: 45,
+  checkedIcon: {
+    backgroundColor: '#4FDFFF',
+    borderRadius: 3,
+    display: 'block',
+    width: 20,
+    height: 20,
+    'input:hover ~ &': {
+      backgroundColor: '#106ba3',
+    },
+    '&:before': {
+      width: 22,
+      height: 8,
+      display: 'block',
+      content: "''",
+      position: 'relative',
+      borderBottom: '4px solid #000000',
+      borderLeft: '4px solid #000000',
+      transform: 'rotate(-45deg)',
+      left: 2,
+      top: 2,
+    },
   },
-  visibilityIconButton: {
-    padding: 0,
+  submitButton: {
+    padding: '16px 44px',
+    borderRadius: 50,
+    marginRight: 0,
+    alignSelf: 'flex-end',
+    backgroundColor: '#FF5C00',
+    '& .MuiButton-label': {
+      color: theme.palette.common.white,
+      fontWeight: 700,
+      fontSize: '18px',
+      fontFamily: 'Raleway',
+    },
+    '&:hover': {
+      backgroundColor: '#ffaa00',
+    },
   },
-  visibilityIcon: {
-    fontSize: 34,
-    color: 'black',
+  congratulationContainer: {
+    '& .swal2-popup': {
+      width: 480,
+    },
+  },
+  congratulationTitleText: {
+    fontFamily: 'Raleway',
+    fontWeight: 500,
+    fontSize: '64px',
+    lineHeight: '66px',
+    marginTop: '-25px', // -15px and -35px, without 90px
+  },
+  congratulationSubText: {
+    fontFamily: 'Raleway',
+    fontWeight: 500,
+    fontSize: '24px',
+    lineHeight: '38px',
+    padding: '0 -20px',
+    marginTop: '-22px',
+  },
+  congratulationButton: {
+    padding: '14px 60px',
+    borderRadius: 50,
+    backgroundColor: '#4FDFFF',
+    color: theme.palette.common.white,
+    fontWeight: 700,
+    fontSize: '18px',
+    fontFamily: 'Raleway',
+    margin: '10px 0 60px 0', // '10px 0 40px or 60px 0' bottom, without '18px 0 71px 0'
+    '&:hover': {
+      backgroundColor: '#106ba3',
+      cursor: 'pointer',
+    },
   },
 }));
