ff6a5761fe027abe414eb1c51f65e52602d7798b
Saashkai <saashkaai@gmail.com>
2021-08-04 19:14:21 +0300

    Implemented task

--- src/components/Posts/Cards/ImportantPostPreviewCard/ImportantPostPreviewCard.tsx
+++ src/components/Posts/Cards/ImportantPostPreviewCard/ImportantPostPreviewCard.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { useHistory } from 'react-router-dom';
-import Typography from '@material-ui/core/Typography';
 import { useStyles } from './ImportantPostPreviewCard.styles';
 import { IPostPreviewCardProps } from '../types';
 import background from '../mock_img_slider_bg.png';
@@ -14,49 +13,10 @@ export const ImportantPostPreviewCard: React.FC<IPostPreviewCardProps> = (
     : background;
   const classes = useStyles({ backgroundImageUrl: bgImageURL, size });
   const history = useHistory();
-  const author = post.author.id || '';
 
   const goPostPage = () => {
     history.push(`/posts/${post.id}`);
   };
 
-  const goExpertPage = () => {
-    history.push(`/experts/${author}`);
-  };
-
-  return (
-    <div className={classes.root}>
-      <Typography
-        component="p"
-        variant="h4"
-        onClick={goExpertPage}
-        className={classes.authorsName}
-      >
-        {post.author.firstName} {post.author.lastName}
-      </Typography>
-      <Typography
-        component="p"
-        variant="subtitle2"
-        className={classes.authorsDetails}
-      >
-        {post.author.mainInstitution?.name}
-      </Typography>
-      <Typography
-        variant="h2"
-        component="p"
-        onClick={goPostPage}
-        className={classes.title}
-      >
-        {post.title}
-      </Typography>
-      <Typography
-        variant="body1"
-        component="p"
-        onClick={goPostPage}
-        className={classes.preview}
-      >
-        {post.preview}
-      </Typography>
-    </div>
-  );
+  return <div className={classes.root} onClick={goPostPage}></div>;
 };
