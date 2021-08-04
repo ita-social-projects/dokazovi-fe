7830f1807f6b823026dd7ae39b7eb7692d284e91
Saashkai <saashkaai@gmail.com>
2021-08-04 19:39:59 +0300

    fixed linter errors

--- src/components/Posts/PostInfo/PostInfo.tsx
+++ src/components/Posts/PostInfo/PostInfo.tsx
@@ -1,3 +1,6 @@
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+/* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
 import React from 'react';
 import { Skeleton } from '@material-ui/lab';
 import VisibilityIcon from '@material-ui/icons/Visibility';
