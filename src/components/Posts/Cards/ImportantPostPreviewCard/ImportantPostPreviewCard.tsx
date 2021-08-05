ff260dee9f6086ec77d71c3b7cec2728edbbbacb
NabokinAlexandr <56695491+NabokinAlexandr@users.noreply.github.com>
2021-08-05 11:09:30 +0300

    #489 / remove text fields from carousel & link with post (#493)
    * Implemented task
    
    * fixed eslint warning
    
    * removed obsolete tests

--- src/components/Posts/Cards/ImportantPostPreviewCard/ImportantPostPreviewCard.tsx
+++ src/components/Posts/Cards/ImportantPostPreviewCard/ImportantPostPreviewCard.tsx
@@ -1,6 +1,7 @@
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+/* eslint-disable jsx-a11y/no-static-element-interactions */
 import React from 'react';
 import { useHistory } from 'react-router-dom';
-import Typography from '@material-ui/core/Typography';
 import { useStyles } from './ImportantPostPreviewCard.styles';
 import { IPostPreviewCardProps } from '../types';
 import background from '../mock_img_slider_bg.png';
@@ -14,49 +15,16 @@ export const ImportantPostPreviewCard: React.FC<IPostPreviewCardProps> = (
     : background;
   const classes = useStyles({ backgroundImageUrl: bgImageURL, size });
   const history = useHistory();
-  const author = post.author.id || '';
 
   const goPostPage = () => {
     history.push(`/posts/${post.id}`);
   };
 
-  const goExpertPage = () => {
-    history.push(`/experts/${author}`);
-  };
-
   return (
-    <div className={classes.root}>
-      <Typography
-        component="p"
-        variant="h4"
-        onClick={goExpertPage}
-        className={classes.authorsName}
-      >
-        {post.author.firstName} {post.author.lastName}
-      </Typography>
-      <Typography
-        component="p"
-        variant="subtitle2"
-        className={classes.authorsDetails}
-      >
-        {post.author.mainInstitution?.name}
-      </Typography>
-      <Typography
-        variant="h2"
-        component="p"
-        onClick={goPostPage}
-        className={classes.title}
-      >
-        {post.title}
-      </Typography>
-      <Typography
-        variant="body1"
-        component="p"
-        onClick={goPostPage}
-        className={classes.preview}
-      >
-        {post.preview}
-      </Typography>
-    </div>
+    <div
+      className={classes.root}
+      data-testid="post-slider"
+      onClick={goPostPage}
+    />
   );
 };
