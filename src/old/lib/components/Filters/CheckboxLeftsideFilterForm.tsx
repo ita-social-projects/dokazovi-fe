3b1d277774eeb1fb90c45232e712873e6236076e
Danylo Shevchenko <60629098+shedanny@users.noreply.github.com>
2021-08-12 23:34:27 +0300

    Bugfix #153 and #154 Fix filters on the author`s profile page when no materials is added by the user (#507)
    * Fix filters when no materials is added by the user
    
    * Refactor bugfix#153 and #154

--- src/old/lib/components/Filters/CheckboxLeftsideFilterForm.tsx
+++ src/old/lib/components/Filters/CheckboxLeftsideFilterForm.tsx
@@ -216,13 +216,10 @@ export const CheckboxLeftsideFilterForm: React.FC<ICheckboxLeftsideFilterFormPro
     setDisabledCheckBoxesIds(arrOfDisabledIds);
   }, [disabledPostTypes, disabledDirections]);
 
-  let theOnlyAvailableFilter = false;
-  if (
-    disabledCheckBoxesIds &&
-    possibleFilters.length - disabledCheckBoxesIds.length === 1
-  ) {
-    theOnlyAvailableFilter = true;
-  }
+  const theOnlyAvailableFilter =
+    !!disabledCheckBoxesIds &&
+    (possibleFilters.length - disabledCheckBoxesIds.length === 0 ||
+      possibleFilters.length - disabledCheckBoxesIds.length === 1);
 
   useEffect(() => {
     if (theOnlyAvailableFilter && setTheOnlyAvailableFilter) {
