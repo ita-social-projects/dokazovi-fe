00c7b55fe04ade628d8709f86b334d781148742c
Bohdan <bodiasharko@gmail.com>
2021-08-20 20:37:25 +0300

    edit regular expression

--- src/old/lib/constants/editors.ts
+++ src/old/lib/constants/editors.ts
@@ -6,4 +6,4 @@ export const MAX_PREVIEW_LENGTH = 150;
 export const MIN_TITLE_LENGTH = 10;
 export const MIN_CONTENT_LENGTH = 15;
 export const CHECK_REG_EXP = /^[а-яєїіґ\d\s\W]+$/i;
-export const CLEAR_HTML_REG_EXP = /<\/?[^>]+(>|$)|&[a-z]+;/g;
+export const CLEAR_HTML_REG_EXP = /<\/?[^>]+(>|$)|&[a-z0-9]+;/ig;
