659c7d03f66a9816d3064801ba2476663f691423
Bohdan <bodiasharko@gmail.com>
2021-08-24 19:07:09 +0300

    edit text material card at editor when user edit post

--- src/old/lib/constants/editors.ts
+++ src/old/lib/constants/editors.ts
@@ -1,9 +1,9 @@
-export const CONTENT_DEBOUNCE_TIMEOUT = 0; // 1000
-export const PREVIEW_DEBOUNCE_TIMEOUT = 0; // 500
+export const CONTENT_DEBOUNCE_TIMEOUT = 1000;
+export const PREVIEW_DEBOUNCE_TIMEOUT = 500;
 
 export const MAX_PREVIEW_LENGTH = 150;
 
 export const MIN_TITLE_LENGTH = 10;
 export const MIN_CONTENT_LENGTH = 15;
 export const CHECK_REG_EXP = /^[а-яєїіґ\d\s\W]+$/i;
-export const CLEAR_HTML_REG_EXP = /<\/?[^>]+(>|$)|&[a-z0-9]+;/ig;
+export const CLEAR_HTML_REG_EXP = /<\/?[^>]+(>|$)|&[a-z0-9]+;/gi;
