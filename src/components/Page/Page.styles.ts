412fa102caaf4906633511b5be1585919662cb65
shedanny <shedanny23@gmail.com>
2021-08-31 02:11:59 +0300

    Change Header, Footer and Page spacing

--- src/components/Page/Page.styles.ts
+++ src/components/Page/Page.styles.ts
@@ -4,7 +4,10 @@ export const useStyles = makeStyles(
   (theme: Theme) => ({
     page: {
       margin: theme.spacing(10, 'auto'),
-      padding: theme.spacing(0, 5),
+      padding: theme.spacing(0, 10),
+      [theme.breakpoints.down('xs')]: {
+        padding: theme.spacing(0, 5),
+      },
     },
   }),
   { name: 'Page' },
