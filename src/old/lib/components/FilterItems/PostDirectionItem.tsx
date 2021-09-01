45e48602e5904576f5aa81d4783fa461d917f91b
shedanny <shedanny23@gmail.com>
2021-09-01 17:33:06 +0300

    Update import

--- src/old/lib/components/FilterItems/PostDirectionItem.tsx
+++ src/old/lib/components/FilterItems/PostDirectionItem.tsx
@@ -1,7 +1,6 @@
 import React, { useContext } from 'react';
 import { Typography } from '@material-ui/core';
-import { ScreenContext } from 'old/provider/MobileProvider/ScreenContext';
-
+import { ScreenContext } from '../../../provider/MobileProvider/ScreenContext';
 export interface IPostDirectionItemProps {
   labelName?: string;
   handleClick?: () => void;
