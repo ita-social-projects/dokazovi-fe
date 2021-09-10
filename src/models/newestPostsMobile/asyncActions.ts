4b696ca6e011cb4e85d5a84120a67b5845b740fa
Bohdan <bodiasharko@gmail.com>
2021-09-10 17:44:56 +0300

    edit mobile view

--- src/models/newestPostsMobile/asyncActions.ts
+++ src/models/newestPostsMobile/asyncActions.ts
@@ -3,11 +3,13 @@ import { getNewestPostsForMobile } from '../../old/lib/utilities/API/api';
 
 export const fetchNewestMobile = createAsyncThunk(
   'materials/mobile',
-  async () => {
+  async (_, { rejectWithValue }) => {
     try {
       const res = await getNewestPostsForMobile();
       const [expertOpinion, media, translation, video] = res.data.content;
       return [expertOpinion, translation, media, video];
-    } catch (err) {}
+    } catch (err) {
+      rejectWithValue(err.response?.data);
+    }
   },
 );
