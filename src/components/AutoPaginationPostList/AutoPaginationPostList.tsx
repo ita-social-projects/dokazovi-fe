54ee349e4406bd20ee1f5f0fdb9254c324c5eaa3
Bohdan <bodiasharko@gmail.com>
2021-09-11 11:38:16 +0300

    edit mobile view

--- src/components/AutoPaginationPostList/AutoPaginationPostList.tsx
+++ src/components/AutoPaginationPostList/AutoPaginationPostList.tsx
@@ -9,17 +9,19 @@ interface IAutoPaginationPostListProps {
 export const AutoPaginationPostList: React.FC<IAutoPaginationPostListProps> = ({
   posts,
 }) => {
-  const [page, setPage] = useState(4);
+  const [page, setPage] = useState(3);
 
   const lastElement = useRef<HTMLDivElement>(null);
   const observer = useRef<IntersectionObserver>();
+
+  const lastTwoPosts = posts.slice(page, page + 2);
 
   useEffect(() => {
     if (observer.current) observer.current.disconnect();
     if (lastElement.current) {
       observer.current = new IntersectionObserver((entries) => {
         if (entries[0].isIntersecting) {
-          setPage((p) => p + 1);
+          setPage((p) => p + 4);
         }
       });
       observer.current.observe(lastElement.current);
@@ -29,7 +31,11 @@ export const AutoPaginationPostList: React.FC<IAutoPaginationPostListProps> = ({
   return (
     <div>
       <PostsList postsList={posts.slice(0, page)} />
-      <div ref={lastElement} style={{ height: 1 }} />
+      {Boolean(lastTwoPosts.length) && (
+        <div ref={lastElement}>
+          <PostsList postsList={lastTwoPosts} />
+        </div>
+      )}
     </div>
   );
 };
