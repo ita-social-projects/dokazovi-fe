214ce5277c37aea7fb9c21fb734c7e3e4634dfdb
Dmytro Pavliuk <pavlyukdm@gmail.com>
2021-10-10 23:24:29 +0300

    add sortKeys to tableHeadContent

--- src/views/Profile/MaterialsView/MaterialsTableHead.tsx
+++ src/views/Profile/MaterialsView/MaterialsTableHead.tsx
@@ -1,16 +1,44 @@
 import React from 'react';
 import { TableCell, TableHead } from '@material-ui/core';
+import { SortBy } from 'models/adminlab/types';
 
 const tableHeadContent = [
-  { title: 'Id' },
-  { title: 'Заголовок' },
-  { title: 'Статус' },
-  { title: 'Дата зміни статусу' },
-  { title: 'Тема' },
-  { title: 'Автор' },
-  { title: 'К-сть переглядів, що відображається на сайті' },
-  { title: 'Реальна к-сть переглядів' },
-  { title: 'Дії' },
+  {
+    sortKey: SortBy.post_id,
+    title: 'Id',
+  },
+  {
+    sortKey: SortBy.title,
+    title: 'Заголовок',
+  },
+  {
+    sortKey: SortBy.status,
+    title: 'Статус',
+  },
+  {
+    sortKey: SortBy.modifide_at,
+    title: 'Дата зміни статусу',
+  },
+  {
+    sortKey: SortBy.type_id,
+    title: 'Тема',
+  },
+  {
+    sortKey: SortBy.post_id,
+    title: 'Автор',
+  },
+  {
+    sortKey: SortBy.post_id,
+    title: 'К-сть переглядів, що відображається на сайті',
+  },
+  {
+    sortKey: SortBy.post_id,
+    title: 'Реальна к-сть переглядів',
+  },
+  {
+    sortKey: SortBy.post_id,
+    title: 'Дії',
+  },
 ];
 
 const MaterailsTableHead: React.FC = () => {
