3fe6da102827008a3b69d2d040064c55a4511317
Dmytro Pavliuk <pavlyukdm@gmail.com>
2021-10-12 20:17:33 +0300

    add pagination skeleton

--- src/views/Profile/MaterialsView/MaterialsTablePagination.tsx
+++ src/views/Profile/MaterialsView/MaterialsTablePagination.tsx
@@ -1,7 +1,18 @@
 import React from 'react';
+import { Pagination } from '@material-ui/lab';
+import { useStyles } from './styles/MaterialsTablePagination.styles';
 
 const MaterialsTablePagination: React.FC = () => {
-  return <div>pagination</div>;
+  const classes = useStyles();
+
+  return (
+    <Pagination
+      className={classes.root}
+      count={75}
+      page={1}
+      onChange={() => console.log('click')}
+    />
+  );
 };
 
 export default MaterialsTablePagination;
