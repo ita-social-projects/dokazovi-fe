de2c205cfa1da296a2870b0e417f66f54551dfcd
Ivan Danyliuk <80050483+IvanDanyliuk@users.noreply.github.com>
2021-10-13 13:12:53 +0300

    Create tests for ExpertInfo, ExpertProfileView, and AutoPaginationExpertsList (#613)

---
+++ src/old/modules/experts/components/__test__/ExpertInfo.test.tsx
@@ -0,0 +1,63 @@
+import React from 'react';
+import { screen, render } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
+import { IExpert } from 'old/lib/types';
+import ExpertInfo from '../ExpertInfo';
+
+const EXPERT_INFO_MOCK: IExpert = {
+  id: 1,
+  firstName: 'John',
+  lastName: 'Doe',
+  avatar: 'avatar',
+  phone: '+38(044)1111111',
+  email: 'j.doe@gmail.com',
+  bio: 'Lorem ipsum dolor sit amet',
+  socialNetwork: 'facebook/link',
+};
+
+const EXPERT_INFO_WITHOUT_CONTACTS_MOCK: IExpert = {
+  id: 2,
+  firstName: 'Linda',
+  lastName: 'Doe',
+  avatar: 'avatar',
+  phone: '+38(044)7777777',
+  bio: 'Lorem ipsum dolor sit amet consectur',
+};
+
+describe('ExpertsInfo component renders with all props', () => {
+  beforeEach(() => {
+    render(
+      <MemoryRouter>
+        <ExpertInfo expert={EXPERT_INFO_MOCK} />
+      </MemoryRouter>,
+    );
+  });
+
+  it('ExpertsInfo component renders full name', () => {
+    expect(screen.getByText('John Doe')).toBeInTheDocument();
+  });
+
+  it('ExpertsInfo avatar renders', () => {
+    expect(screen.getByRole('img')).toBeInTheDocument();
+  });
+
+  it('ExpertsInfo bio renders', () => {
+    expect(screen.getByText('Lorem ipsum dolor sit amet')).toBeInTheDocument();
+  });
+
+  it("Expert's contact info renders", () => {
+    expect(screen.getByText('j.doe@gmail.com')).toBeInTheDocument();
+    expect(screen.getByText('facebook/link')).toBeInTheDocument();
+  });
+});
+
+describe('Render ExpertInfo component without contact information', () => {
+  it('Render ExpertInfo without contact information', () => {
+    render(
+      <MemoryRouter>
+        <ExpertInfo expert={EXPERT_INFO_WITHOUT_CONTACTS_MOCK} />
+      </MemoryRouter>,
+    );
+    expect(screen.queryByText('j.doe@gmail.com')).toBeNull();
+  });
+});
---
+++ src/old/modules/experts/components/__test__/ExpertProfileView.test.tsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import { screen, render } from '@testing-library/react';
+import { Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { createMemoryHistory } from 'history';
+import { IExpert } from 'old/lib/types';
+import { store } from '../../../../../models/store';
+import ExpertProfileView from '../ExpertProfileView';
+
+const history = createMemoryHistory();
+
+const TEST_DATA: IExpert = {
+  id: 1,
+  firstName: 'John',
+  lastName: 'Doe',
+  avatar: 'avatar',
+  qualification: 'ophtalmology',
+  phone: '+38(044)7777777',
+  email: 'j.doe@gmail.com',
+  bio: 'Lorem ipsum dolor sit amet consectur',
+  socialNetwork: 'www.facebook.com',
+};
+
+describe('ExpertsProfileView component renders correctly', () => {
+  beforeEach(() => {
+    render(
+      <Provider store={store}>
+        <Router history={history}>
+          <ExpertProfileView expert={TEST_DATA} />
+        </Router>
+      </Provider>,
+    );
+  });
+
+  it('Expert full name renders', () => {
+    expect(screen.getByText('John Doe')).toBeInTheDocument();
+  });
+
+  it('Expert avatar renders', () => {
+    expect(screen.getByRole('img')).toBeInTheDocument();
+  });
+
+  it('Expert bio renders', () => {
+    expect(
+      screen.getByText('Lorem ipsum dolor sit amet consectur'),
+    ).toBeInTheDocument();
+  });
+});
