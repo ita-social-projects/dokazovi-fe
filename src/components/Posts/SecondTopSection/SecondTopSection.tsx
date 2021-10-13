4bf8d4fe0554b6f5f56c71b999bde1d102a7e65b
OksanLan <75547527+OksanLan@users.noreply.github.com>
2021-10-13 13:12:34 +0300

    cover with tests SecondTopSection Component (#615)

--- src/components/Posts/SecondTopSection/SecondTopSection.tsx
+++ src/components/Posts/SecondTopSection/SecondTopSection.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { useStyles } from './SecondTopSection.styles';
 
-interface ISecondTopSection {
+export interface ISecondTopSection {
   author: {
     firstName: string;
     id: number;
@@ -21,15 +21,19 @@ export default function SecondTopSection({
   const { id, firstName, lastName, bio } = author;
   const authorFullName = `${firstName} ${lastName}`;
   return (
-    <Box className={classes.root}>
+    <Box className={classes.root} data-testid="block">
       {origin !== 3 ? (
         <>
-          <Link to={`/experts/${id}`}>
+          <Link to={`/experts/${id}`} data-testid="link">
             <Typography variant="h4" className={classes.authorName}>
               {authorFullName}
             </Typography>
           </Link>
-          <Typography color="textSecondary" className={classes.authorBio}>
+          <Typography
+            color="textSecondary"
+            className={classes.authorBio}
+            data-testid="typography"
+          >
             {bio}
           </Typography>
         </>
