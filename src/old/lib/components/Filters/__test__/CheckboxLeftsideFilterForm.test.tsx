1b3c09442106e0c4f1fcaa7509fd3a7c6a42afee
IvanDanyliuk <ivandaniliuk@gmail.com>
2021-10-21 21:09:14 +0300

    cover with tests filters

--- src/old/lib/components/Filters/__test__/CheckboxLeftsideFilterForm.test.tsx
+++ src/old/lib/components/Filters/__test__/CheckboxLeftsideFilterForm.test.tsx
@@ -4,7 +4,7 @@ import { Provider } from 'react-redux';
 import { store } from 'models/store';
 import { Router } from 'react-router-dom';
 import { createMemoryHistory } from 'history';
-import { filtersStateEnum, QueryTypeEnum } from '../../../../lib/types';
+import { filtersStateEnum, QueryTypeEnum } from '../../../types';
 import { CheckboxLeftsideFilterForm } from '../CheckboxLeftsideFilterForm';
 
 const history = createMemoryHistory();
