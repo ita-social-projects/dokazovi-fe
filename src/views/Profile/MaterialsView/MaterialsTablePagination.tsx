3109fe4b08e107b46282fb4ae68cb2e843da4b1f
Dmytro Pavliuk <pavlyukdm@gmail.com>
2021-10-21 11:40:57 +0300

    implement basic ui pagination

--- src/views/Profile/MaterialsView/MaterialsTablePagination.tsx
+++ src/views/Profile/MaterialsView/MaterialsTablePagination.tsx
@@ -1,16 +1,27 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { selectMeta, setPage } from 'models/adminlab';
 import { Pagination } from '@material-ui/lab';
 import { useStyles } from './styles/MaterialsTablePagination.styles';
 
 const MaterialsTablePagination: React.FC = () => {
   const classes = useStyles();
+  const dispatch = useDispatch();
+  const { page } = useSelector(selectMeta);
+
+  const handleChangePage = (
+    event: React.ChangeEvent<unknown>,
+    value: number,
+  ) => {
+    dispatch(setPage({ page: value }));
+  };
 
   return (
     <Pagination
       className={classes.root}
       count={75}
-      page={1}
-      onChange={() => console.log('click')}
+      page={page}
+      onChange={handleChangePage}
     />
   );
 };
