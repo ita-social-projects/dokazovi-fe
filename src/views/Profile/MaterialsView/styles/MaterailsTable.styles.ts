e08671d3ae9d0825fb3001fab88f3e644915a535
Dmytro Pavliuk <pavlyukdm@gmail.com>
2021-10-21 12:36:00 +0300

    add styling to pagination

--- src/views/Profile/MaterialsView/styles/MaterailsTable.styles.ts
+++ src/views/Profile/MaterialsView/styles/MaterailsTable.styles.ts
@@ -2,8 +2,9 @@ import { makeStyles } from '@material-ui/core';
 
 export const useStyles = makeStyles(() => ({
   table: {
+    marginBottom: '1rem',
     '& .MuiTableCell-root': {
-      padding: '10px',
+      padding: '9px',
       fontFamily: 'Raleway',
       fontWeight: '500',
     },
