c05ed643c2a8ae927615462324ef67a3b4be7214
Dmytro Pavliuk <pavlyukdm@gmail.com>
2021-10-21 17:19:29 +0300

    set real total pages count

--- src/views/Profile/MaterialsView/MaterialsTablePagination.tsx
+++ src/views/Profile/MaterialsView/MaterialsTablePagination.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { selectMeta, setPage } from 'models/adminlab';
+import { selectAdminlab, selectMeta, setPage } from 'models/adminlab';
 import { Pagination } from '@material-ui/lab';
 import { useStyles } from './styles/MaterialsTablePagination.styles';
 
 const MaterialsTablePagination: React.FC = () => {
   const classes = useStyles();
   const dispatch = useDispatch();
+  const { totalPages } = useSelector(selectAdminlab);
   const { page } = useSelector(selectMeta);
 
   const handleChangePage = (
@@ -19,7 +20,7 @@ const MaterialsTablePagination: React.FC = () => {
   return (
     <Pagination
       className={classes.root}
-      count={75}
+      count={totalPages}
       page={page + 1}
       onChange={handleChangePage}
     />
