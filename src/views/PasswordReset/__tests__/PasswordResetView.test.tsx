db0184629d5439e65b14196de47d2be7a7b41584
OksanLan <75547527+OksanLan@users.noreply.github.com>
2021-10-29 17:57:28 +0300

    cover with tests PasswordResetView  (#628)
    * cover with tests PasswordResetView
    
    * fix mocked data
    
    * fix mocked data
    
    * mocked axios
    
    * Revert "fix mocked data"
    
    This reverts commit 16002b5eab8ba307ebda71ad2418469522f26fe7.
    
    * Revert "mocked axios"
    
    This reverts commit ac4b729733e07b33afb1a4499966a698295229ad.
    
    * Revert "Revert "fix mocked data""
    
    This reverts commit 74d71948d5a58b4259c117e2e92877cbc7ca282e.
    
    * refactor PasswordResetView tests

---
+++ src/views/PasswordReset/__tests__/PasswordResetView.test.tsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import { render, screen, waitFor, fireEvent } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import PasswordResetView from '../PasswordResetView';
+import { resetPasswordRequest } from '../../../old/lib/utilities/API/api';
+
+jest.mock('../../../old/lib/utilities/API/api', () => ({
+  resetPasswordRequest: jest.fn(),
+}));
+
+describe('PasswordResetView tests', () => {
+  it('should PasswordResetView component render', async () => {
+    const { asFragment } = render(<PasswordResetView />);
+
+    expect(
+      await screen.findByTestId('password-reset-view'),
+    ).toBeInTheDocument();
+
+    expect(asFragment()).toMatchSnapshot();
+  });
+
+  it('should render success view', async () => {
+    const { getByTestId } = render(<PasswordResetView />);
+
+    const basicInput = getByTestId('basic-input');
+
+    fireEvent.blur(basicInput);
+    userEvent.type(basicInput, 'test@mail.com');
+
+    expect(screen.queryByTestId('submittedContainer')).not.toBeInTheDocument();
+    userEvent.click(screen.getByText('Підтвердити зміни'));
+
+    await waitFor(() =>
+      expect(screen.getByTestId('submittedContainer')).toBeInTheDocument(),
+    );
+
+    expect(resetPasswordRequest).toHaveBeenCalled();
+  });
+
+  it('should render previous step', async () => {
+    const { getByTestId } = render(<PasswordResetView />);
+
+    const basicInput = getByTestId('basic-input');
+
+    fireEvent.blur(basicInput);
+    userEvent.type(basicInput, 'test@mail.com');
+    userEvent.click(screen.getByText('Підтвердити зміни'));
+
+    await waitFor(() =>
+      expect(screen.getByText('Спробувати ще раз')).toBeInTheDocument(),
+    );
+
+    userEvent.click(screen.getByText('Спробувати ще раз'));
+
+    await waitFor(() =>
+      expect(screen.queryByText('Спробувати ще раз')).not.toBeInTheDocument(),
+    );
+  });
+});
