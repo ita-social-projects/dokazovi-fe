23fb99f333a8a2c9bf8760c81377edeb37619c7a
Anastasia <nastia.shrai@gmail.com>
2021-11-01 17:20:09 +0200

    fix: fix typo

--- src/old/lib/components/Errors/Page404.test.tsx
+++ src/old/lib/components/Errors/Page404.test.tsx
@@ -3,7 +3,7 @@ import { render } from '@testing-library/react';
 import Page404 from './Page404';
 
 describe('Page404', () => {
-  it('render component and match snapshot', () => {
+  it('should render component and match snapshot', () => {
     const { asFragment } = render(<Page404 />);
     expect(asFragment()).toMatchSnapshot();
   });
