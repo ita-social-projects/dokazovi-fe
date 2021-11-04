7652c0e4ff99b80bebccb66d97624a007d1c4b88
IvanDanyliuk <ivandaniliuk@gmail.com>
2021-11-04 22:44:45 +0200

    Removed unused methods

--- src/components/Header/__test__/PostCreationMenu.test.tsx
+++ src/components/Header/__test__/PostCreationMenu.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, screen, fireEvent, waitFor } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
 import { PostCreationMenu } from '../PostCreationMenu';
 
