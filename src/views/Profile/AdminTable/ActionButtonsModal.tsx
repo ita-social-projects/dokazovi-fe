f2a2161485e70e1330655694349c9e6c270fa4d6
Dmytro Pavliuk <pavlyukdm@gmail.com>
2021-11-16 19:37:58 +0200

    add action modals file

---
+++ src/views/Profile/AdminTable/ActionButtonsModal.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import { Button, DialogActions, DialogContent } from '@material-ui/core';
+import { useTranslation } from 'react-i18next';
+import Dialog from '@material-ui/core/Dialog';
+import { langTokens } from '../../../locales/localizationInit';
+
+export interface IModalSettings {
+  title?: string;
+  content?: string | JSX.Element;
+  onConfirmButtonClick?: () => void;
+}
+
+interface IActionButtonsModal {
+  open: boolean;
+  onClose: () => void;
+  modal: IModalSettings;
+}
+
+const ActionButtonsModal: React.FC<IActionButtonsModal> = ({
+  open,
+  onClose,
+  modal,
+}) => {
+  const { t } = useTranslation();
+
+  return (
+    <Dialog open={open} onClose={onClose}>
+      <DialogTitle>{modal.title}</DialogTitle>
+      {modal.content && <DialogContent>{modal.content}</DialogContent>}
+      <DialogActions>
+        <Button onClick={modal.onConfirmButtonClick}>
+          {t(langTokens.common.yes)}
+        </Button>
+        <Button onClick={onClose}>{t(langTokens.common.no)}</Button>
+      </DialogActions>
+    </Dialog>
+  );
+};
+
+export default ActionButtonsModal;
