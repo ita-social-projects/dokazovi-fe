2e7cac073d7935829f4da7f1a8237eb1614c9c17
IvanDanyliuk <ivandaniliuk@gmail.com>
2021-11-23 21:14:02 +0200

    Fix warnings

--- src/old/lib/components/Users/__test__/LoginModal.test.tsx
+++ src/old/lib/components/Users/__test__/LoginModal.test.tsx
@@ -11,25 +11,22 @@ import { Provider } from 'react-redux';
 import { store } from 'models/store';
 import { Router } from 'react-router-dom';
 import MockAdapter from 'axios-mock-adapter';
-import * as api from '../../../utilities/API/api';
 import Swal from 'sweetalert2';
 import { createMemoryHistory } from 'history';
+import * as api from '../../../utilities/API/api';
 import { LoginModal } from '../LoginModal';
 
 const history = createMemoryHistory();
 
-const testInputData = ['testmail@mail.com', 'testpassword'];
-
 const mockedAxios = new MockAdapter(api.instance);
 
-const submitLoginForm = (inputData) => {
+const submitLoginForm = () => {
   const openMenuButton = screen.getByRole('button', { name: 'Увійти' });
   fireEvent.click(openMenuButton);
 
   const inputs = screen.getAllByTestId('basic-input');
-  inputs.forEach((input, i) => {
-    userEvent.type(input, inputData[i]);
-  });
+  userEvent.type(inputs[0], 'testmail@mail.com');
+  userEvent.type(inputs[1], 'testpassword');
 
   const submitButton = screen.getByRole('button', { name: 'Увійти' });
   fireEvent.submit(submitButton);
@@ -60,14 +57,12 @@ describe('LoginModal tests', () => {
 
   it("should submit authorization form after passing email and password data by clicking on 'Увійти' button", async () => {
     mockedAxios.onPost('/auth/login').replyOnce(200, {});
-    const swalFireMock = jest
-      .spyOn(Swal, 'fire')
-      .mockResolvedValueOnce({
-        isConfirmed: true,
-        isDenied: false,
-        isDismissed: false,
-      });
-    submitLoginForm(testInputData);
+    const swalFireMock = jest.spyOn(Swal, 'fire').mockResolvedValueOnce({
+      isConfirmed: true,
+      isDenied: false,
+      isDismissed: false,
+    });
+    submitLoginForm();
     await waitFor(() => {
       expect(swalFireMock).toHaveBeenCalled();
       expect(history.location.pathname).toBe('/');
@@ -79,7 +74,7 @@ describe('LoginModal tests', () => {
     const loginFn = jest
       .spyOn(api, 'login')
       .mockRejectedValueOnce({ response: { data: { status: 'Error' } } });
-    submitLoginForm(testInputData);
+    submitLoginForm();
     await waitFor(() => {
       expect(loginFn).toHaveBeenCalled();
     });
