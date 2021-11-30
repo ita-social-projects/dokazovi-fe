65abb32e6c21c0d94a3a2bacf0f341d79790f1ae
IvanDanyliuk <ivandaniliuk@gmail.com>
2021-11-30 19:13:00 +0200

    Cover with tests MaterialsViewMobile component

---
+++ src/old/modules/materials/components/__test__/MaterialsViewMobile.test.tsx
@@ -0,0 +1,74 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
+import { LoadingStatusEnum } from 'old/lib/types';
+import MaterialsViewMobile from '../MaterialsViewMobile';
+
+const SelectedTypes = () => {
+  return <div>Selected Types</div>;
+};
+const FilterCheckboxes = () => {
+  return <div>FilterCheckboxes</div>;
+};
+const LoadMoreButton = () => {
+  return <button>Load More</button>;
+};
+
+const PROPS_MOCK = {
+  page: 1,
+  header: 'Materials',
+  loading: LoadingStatusEnum.succeeded,
+  materials: [
+    {
+      author: {
+        firstName: 'Олег',
+        lastName: 'Петренко',
+      },
+    },
+  ],
+  totalElements: 1,
+  resetPage: jest.fn(),
+  SelectedTypes: <SelectedTypes />,
+  FilterCheckboxes: <FilterCheckboxes />,
+  LoadMoreButton: <LoadMoreButton />,
+};
+
+describe('MaterialsViewMobile tests', () => {
+  it("should render materials list when loading status is 'succeeded'", () => {
+    render(
+      <MemoryRouter>
+        <MaterialsViewMobile
+          page={PROPS_MOCK.page}
+          header={PROPS_MOCK.header}
+          loading={PROPS_MOCK.loading}
+          materials={PROPS_MOCK.materials}
+          totalElements={PROPS_MOCK.totalElements}
+          resetPage={PROPS_MOCK.resetPage}
+          SelectedTypes={PROPS_MOCK.SelectedTypes}
+          FilterCheckboxes={PROPS_MOCK.FilterCheckboxes}
+          LoadMoreButton={PROPS_MOCK.LoadMoreButton}
+        />
+      </MemoryRouter>,
+    );
+    expect(screen.getByText(/Олег Петренко/)).toBeInTheDocument();
+  });
+
+  it("should not render materials list when the loading status is 'pending'", () => {
+    render(
+      <MemoryRouter>
+        <MaterialsViewMobile
+          page={0}
+          header={PROPS_MOCK.header}
+          loading={LoadingStatusEnum.pending}
+          materials={PROPS_MOCK.materials}
+          totalElements={PROPS_MOCK.totalElements}
+          resetPage={PROPS_MOCK.resetPage}
+          SelectedTypes={PROPS_MOCK.SelectedTypes}
+          FilterCheckboxes={PROPS_MOCK.FilterCheckboxes}
+          LoadMoreButton={PROPS_MOCK.LoadMoreButton}
+        />
+      </MemoryRouter>,
+    );
+    expect(screen.getByText(/1 матеріал/)).toBeInTheDocument();
+  });
+});
