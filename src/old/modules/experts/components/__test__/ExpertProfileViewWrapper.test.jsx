1e7e04f74cc0397c2e19df2fcd3344684700f384
OksanLan <75547527+OksanLan@users.noreply.github.com>
2021-12-09 13:46:01 +0200

    cover with tests ExpertProfileViewWrapper (#683)

---
+++ src/old/modules/experts/components/__test__/ExpertProfileViewWrapper.test.jsx
@@ -0,0 +1,147 @@
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+import React from 'react';
+import { render, screen, waitFor } from '@testing-library/react';
+import { combineReducers, configureStore, createSlice } from '@reduxjs/toolkit';
+import { Provider } from 'react-redux';
+import { MemoryRouter } from 'react-router-dom';
+import {
+  mockExpertMaterials,
+  mockProperties,
+  mockMaterials,
+} from './mocks/expertProfileWrapper';
+import ExpertProfileViewWrapper from '../ExpertProfileViewWrapper';
+import { getExpertById } from '../../../../lib/utilities/API/api';
+
+const mockedStore = {
+  expertMaterials: mockExpertMaterials,
+  properties: mockProperties,
+  materials: mockMaterials,
+};
+
+const mockExpertMaterialsSlice = createSlice({
+  name: 'mockExpertMaterials',
+  initialState: mockExpertMaterials,
+  reducers: {},
+});
+
+const mockPropertiesSlice = createSlice({
+  name: 'mockExpertMaterials',
+  initialState: mockProperties,
+  reducers: {},
+});
+
+const mockMaterialsSlice = createSlice({
+  name: 'mockExpertMaterials',
+  initialState: mockMaterials,
+  reducers: {},
+});
+
+const mockStr = configureStore({
+  reducer: combineReducers({
+    expertMaterials: mockExpertMaterialsSlice.reducer,
+    properties: mockPropertiesSlice.reducer,
+    materials: mockMaterialsSlice.reducer,
+  }),
+});
+
+jest.mock('../../../../../models/store', () => ({
+  store: {
+    getState: () => mockedStore,
+  },
+}));
+
+jest.mock('../../../../lib/utilities/API/api', () => ({
+  getExpertById: jest.fn(),
+
+  getActivePostTypes: () =>
+    Promise.resolve({
+      data: [{ id: 1, name: 'Стаття' }],
+    }),
+  getActiveDirections: () =>
+    Promise.resolve({
+      data: [
+        {
+          id: 3,
+          name: 'surgery',
+          label: 'Хірургія',
+          color: '#7aebbf',
+          hasDoctors: true,
+          hasPosts: true,
+        },
+      ],
+    }),
+}));
+
+jest.mock('react-ga');
+
+const mockHistoryPush = jest.fn();
+
+jest.mock('react-router-dom', () => ({
+  ...jest.requireActual('react-router-dom'),
+  useHistory: () => ({
+    push: mockHistoryPush,
+  }),
+  useParams: () => ({ expertId: '10' }),
+  useLocation: () => ({
+    pathname: '/test',
+  }),
+}));
+
+describe('ExpertProfileViewWrapper component tests', () => {
+  it('should render ExpertProfileViewWrapper component', async () => {
+    const { asFragment } = render(
+      <Provider store={mockStr}>
+        <MemoryRouter>
+          <ExpertProfileViewWrapper />
+        </MemoryRouter>
+      </Provider>,
+    );
+
+    await waitFor(() => expect(asFragment()).toMatchSnapshot());
+  });
+
+  it('should render expert profile', async () => {
+    getExpertById.mockImplementation(() =>
+      Promise.resolve({
+        data: {
+          id: 10,
+          firstName: 'Марія',
+          lastName: 'Марієнко',
+          bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+          qualification: 'Лікар вищої категорії',
+          phone: '+380956456969',
+          email: 'masha@mail.com',
+        },
+      }),
+    );
+
+    render(
+      <Provider store={mockStr}>
+        <MemoryRouter>
+          <ExpertProfileViewWrapper />
+        </MemoryRouter>
+      </Provider>,
+    );
+
+    await waitFor(() =>
+      expect(screen.getByTestId('expert-info')).toBeInTheDocument(),
+    );
+  });
+
+  it('should render error 404', async () => {
+    getExpertById.mockImplementation(() => Promise.reject());
+
+    render(
+      <Provider store={mockStr}>
+        <MemoryRouter>
+          <ExpertProfileViewWrapper />
+        </MemoryRouter>
+      </Provider>,
+    );
+
+    await waitFor(() =>
+      expect(mockHistoryPush).toHaveBeenCalledWith(`/error_404`),
+    );
+  });
+});
---
+++ src/old/modules/experts/components/__test__/mocks/expertProfileWrapper.js
@@ -0,0 +1,588 @@
+export const mockExpertMaterials = {
+  data: {
+    postIds: [20, 30],
+    posts: {
+      20: {
+        id: 20,
+        title: 'Fermentum iaculis',
+        preview: 'Fermentum iaculis',
+        content:
+          'Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Proin orci lectus, sodales sed varius non, fringilla non metus. Nam tincidunt libero eu tincidunt lacinia. Sed scelerisque metus at purus pellentesque, eu elementum mauris tempor. Mauris metus mauris, convallis in porttitor sit amet, fermentum vel ipsum. Sed hendrerit volutpat eros, quis commodo ligula aliquet vitae. Nullam gravida quam et ligula facilisis, a volutpat sapien viverra. Integer non porta enim, in blandit leo. Etiam tempor elit lacus, ut venenatis massa rhoncus at. Etiam eu massa rutrum, aliquet arcu at, lobortis ligula. In sit amet nisi nec ex congue lobortis vitae at risus. Praesent elementum accumsan magna, eget auctor ex scelerisque non. Fusce posuere, metus varius malesuada varius, sem velit varius mauris, eget scelerisque mi turpis sit amet dui. Cras pellentesque pulvinar tincidunt. Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+        videoUrl: '',
+        previewImageUrl: '',
+        author: {
+          id: 10,
+          firstName: 'Марія',
+          lastName: 'Марієнко',
+          avatar: 'https://i.pravatar.cc/300?img=16',
+          bio:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat duis.',
+          mainInstitution: {
+            id: 5,
+            name: 'Medical Idea',
+            city: {
+              id: 55,
+              name: 'Бровари',
+            },
+          },
+        },
+        directions: [
+          {
+            id: 5,
+            name: 'virology',
+            label: 'Вірусологія',
+            color: '#da80e8',
+            hasDoctors: true,
+            hasPosts: true,
+          },
+          {
+            id: 3,
+            name: 'surgery',
+            label: 'Хірургія',
+            color: '#7aebbf',
+            hasDoctors: true,
+            hasPosts: true,
+          },
+          {
+            id: 2,
+            name: 'ophthalmology',
+            label: 'Офтальмологія',
+            color: '#98ef50',
+            hasDoctors: true,
+            hasPosts: true,
+          },
+        ],
+        tags: [],
+        type: {
+          id: 1,
+          name: 'Стаття',
+        },
+        status: 'PUBLISHED',
+        origins: [
+          {
+            id: 1,
+            name: 'Думка експерта',
+            parameters: null,
+          },
+        ],
+        createdAt: '20.07.2020',
+        modifiedAt: '26.08.2020',
+        publishedAt: '20.10.2021',
+        importanceOrder: null,
+        importantImageUrl: '',
+      },
+
+      30: {
+        id: 30,
+        title: 'Fermentum iaculis',
+        preview: 'Fermentum iaculis',
+        content:
+          'Duis finibus risus et hendrerit fringilla. Donec varius nisi massa, eu facilisis velit iaculis id. Sed porta lectus eget lectus auctor, at iaculis risus consequat. Morbi lacinia pretium est, pulvinar porttitor lorem facilisis ut. Etiam in nisi justo. Vivamus luctus efficitur mauris, ut placerat lorem imperdiet a. Phasellus condimentum ante quis leo tempor semper. Nam sit amet faucibus orci, et egestas tortor. Maecenas ornare a lorem nec mattis. Etiam ac dapibus quam. Proin consequat laoreet rutrum. Cras et congue ligula.',
+        videoUrl: '',
+        previewImageUrl: '',
+        author: {
+          id: 10,
+          firstName: 'Марія',
+          lastName: 'Марієнко',
+          avatar: 'https://i.pravatar.cc/300?img=16',
+          bio:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat duis.',
+          mainInstitution: {
+            id: 5,
+            name: 'Medical Idea',
+            city: {
+              id: 55,
+              name: 'Бровари',
+            },
+          },
+        },
+        directions: [
+          {
+            id: 1,
+            name: 'covid-19',
+            label: 'Covid-19',
+            color: '#ef5350',
+            hasDoctors: true,
+            hasPosts: true,
+          },
+          {
+            id: 6,
+            name: 'cardiology',
+            label: 'Кардіологія',
+            color: '#00ffff',
+            hasDoctors: true,
+            hasPosts: true,
+          },
+          {
+            id: 2,
+            name: 'ophthalmology',
+            label: 'Офтальмологія',
+            color: '#98ef50',
+            hasDoctors: true,
+            hasPosts: true,
+          },
+        ],
+        tags: [],
+        type: {
+          id: 1,
+          name: 'Стаття',
+        },
+        status: 'PUBLISHED',
+        origins: [
+          {
+            id: 3,
+            name: 'Переклад',
+            parameters: null,
+          },
+        ],
+        createdAt: '20.06.2020',
+        modifiedAt: '26.07.2020',
+        publishedAt: '20.10.2021',
+        importanceOrder: null,
+        importantImageUrl: '',
+      },
+    },
+    meta: {
+      isLastPage: false,
+      pageNumber: 0,
+      totalElements: 13,
+      totalPages: 2,
+    },
+    filters: {
+      page: 0,
+      type: [],
+      directions: [],
+    },
+  },
+  loading: 'succeeded',
+  error: null,
+};
+
+export const mockMaterials = {
+  data: {
+    postIds: [20, 30],
+    posts: {
+      20: {
+        id: 20,
+        title: 'Fermentum iaculis',
+        preview: 'Fermentum iaculis',
+        content:
+          'Fermentum iaculis eu non diam phasellus vestibulum lorem sed. Et pharetra pharetra massa massa ultricies. Malesuada proin libero nunc consequat interdum varius sit. Mauris cursus mattis molestie a. Est ullamcorper eget nulla facilisi etiam dignissim diam. Risus ultricies tristique nulla aliquet enim tortor at. Diam quis enim lobortis scelerisque fermentum. Tempor commodo ullamcorper a lacus vestibulum. Erat nam at lectus urna. Dui id ornare arcu odio ut sem nulla pharetra.',
+        videoUrl: '',
+        previewImageUrl: '',
+        author: {
+          id: 2,
+          firstName: 'Милетий',
+          lastName: 'Чернышов',
+          avatar: 'https://i.pravatar.cc/300?img=68',
+          bio:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam q.',
+          mainInstitution: {
+            id: 1,
+            name: 'Адоніс',
+            city: {
+              id: 190,
+              name: 'Київ',
+            },
+          },
+        },
+        directions: [
+          {
+            id: 5,
+            name: 'virology',
+            label: 'Вірусологія',
+            color: '#da80e8',
+            hasDoctors: true,
+            hasPosts: true,
+          },
+          {
+            id: 3,
+            name: 'surgery',
+            label: 'Хірургія',
+            color: '#7aebbf',
+            hasDoctors: true,
+            hasPosts: true,
+          },
+          {
+            id: 2,
+            name: 'ophthalmology',
+            label: 'Офтальмологія',
+            color: '#98ef50',
+            hasDoctors: true,
+            hasPosts: true,
+          },
+        ],
+        tags: [
+          {
+            id: 1,
+            tag: 'Профілактика',
+          },
+          {
+            id: 6,
+            tag: 'ЕКГ',
+          },
+          {
+            id: 2,
+            tag: 'Комплекс',
+          },
+          {
+            id: 4,
+            tag: 'Допомога',
+          },
+        ],
+        type: {
+          id: 1,
+          name: 'Стаття',
+        },
+        status: 'PUBLISHED',
+        origins: [
+          {
+            id: 1,
+            name: 'Думка експерта',
+            parameters: null,
+          },
+        ],
+        createdAt: '16.02.2021',
+        modifiedAt: '16.02.2021',
+        publishedAt: '20.10.2021',
+        importanceOrder: null,
+        importantImageUrl: '',
+      },
+      30: {
+        id: 30,
+        title: 'Proin nibh nisl condimentum',
+        preview: 'Fermentum iaculis',
+        content:
+          'Ullamcorper malesuada proin libero nunc consequat. Sit amet consectetur adipiscing elit pellentesque habitant morbi tristique. Leo in vitae turpis massa sed. Ultrices eros in cursus turpis.',
+        videoUrl: '',
+        previewImageUrl: '',
+        author: {
+          id: 1,
+          firstName: 'Таржеман',
+          lastName: 'Соколов',
+          avatar: 'https://i.pravatar.cc/300?img=70',
+          bio:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam q.',
+          mainInstitution: {
+            id: 1,
+            name: 'Адоніс',
+            city: {
+              id: 190,
+              name: 'Київ',
+            },
+          },
+        },
+        directions: [
+          {
+            id: 5,
+            name: 'virology',
+            label: 'Вірусологія',
+            color: '#da80e8',
+            hasDoctors: true,
+            hasPosts: true,
+          },
+          {
+            id: 1,
+            name: 'covid-19',
+            label: 'Covid-19',
+            color: '#ef5350',
+            hasDoctors: true,
+            hasPosts: true,
+          },
+          {
+            id: 4,
+            name: 'therapy',
+            label: 'Терапія',
+            color: '#ffee58',
+            hasDoctors: true,
+            hasPosts: true,
+          },
+        ],
+        tags: [
+          {
+            id: 5,
+            tag: 'Міокард',
+          },
+          {
+            id: 4,
+            tag: 'Допомога',
+          },
+        ],
+        type: {
+          id: 2,
+          name: 'Відео',
+        },
+        status: 'PUBLISHED',
+        origins: [
+          {
+            id: 1,
+            name: 'Думка експерта',
+            parameters: null,
+          },
+        ],
+        createdAt: '16.02.2021',
+        modifiedAt: '16.02.2021',
+        publishedAt: '20.10.2021',
+        importanceOrder: null,
+        importantImageUrl: '',
+      },
+    },
+    meta: {
+      isLastPage: false,
+      pageNumber: 0,
+      totalElements: 100,
+      totalPages: 9,
+    },
+    filters: {
+      page: 0,
+      origins: [1, 2, 3],
+      postTypes: [1, 2, 3],
+      directions: [1, 6, 5, 2, 4, 1, 7],
+    },
+  },
+  loading: 'succeeded',
+  error: null,
+};
+
+export const mockProperties = {
+  postTypes: [
+    {
+      id: 1,
+      name: 'Стаття',
+    },
+    {
+      id: 2,
+      name: 'Відео',
+    },
+    {
+      id: 3,
+      name: 'Допис',
+    },
+  ],
+  regions: [
+    {
+      id: 1,
+      name: 'Автономна Республіка Крим',
+      usersPresent: false,
+    },
+    {
+      id: 3,
+      name: 'Волинська область',
+      usersPresent: false,
+    },
+    {
+      id: 5,
+      name: 'Донецька область',
+      usersPresent: false,
+    },
+    {
+      id: 6,
+      name: 'Житомирська область',
+      usersPresent: false,
+    },
+    {
+      id: 7,
+      name: 'Закарпатська область',
+      usersPresent: false,
+    },
+    {
+      id: 8,
+      name: 'Запорізька область',
+      usersPresent: false,
+    },
+    {
+      id: 14,
+      name: 'Миколаївська область',
+      usersPresent: false,
+    },
+    {
+      id: 15,
+      name: 'Одеська область',
+      usersPresent: false,
+    },
+    {
+      id: 16,
+      name: 'Полтавська область',
+      usersPresent: false,
+    },
+    {
+      id: 17,
+      name: 'Рівненська область',
+      usersPresent: false,
+    },
+    {
+      id: 18,
+      name: 'Сумська область',
+      usersPresent: false,
+    },
+    {
+      id: 19,
+      name: 'Тернопільська область',
+      usersPresent: false,
+    },
+    {
+      id: 20,
+      name: 'Харківська область',
+      usersPresent: false,
+    },
+    {
+      id: 21,
+      name: 'Херсонська область',
+      usersPresent: false,
+    },
+    {
+      id: 22,
+      name: 'Хмельницька область',
+      usersPresent: false,
+    },
+    {
+      id: 23,
+      name: 'Черкаська область',
+      usersPresent: false,
+    },
+    {
+      id: 24,
+      name: 'Чернівецька область',
+      usersPresent: false,
+    },
+    {
+      id: 25,
+      name: 'Чернігівська область',
+      usersPresent: true,
+    },
+    {
+      id: 2,
+      name: 'Вінницька область',
+      usersPresent: true,
+    },
+    {
+      id: 4,
+      name: 'Дніпропетровська область',
+      usersPresent: true,
+    },
+    {
+      id: 9,
+      name: 'Івано-Франківська область',
+      usersPresent: true,
+    },
+    {
+      id: 10,
+      name: 'Київська область',
+      usersPresent: true,
+    },
+    {
+      id: 11,
+      name: 'Кіровоградська область',
+      usersPresent: true,
+    },
+    {
+      id: 12,
+      name: 'Луганська область',
+      usersPresent: true,
+    },
+    {
+      id: 13,
+      name: 'Львівська область',
+      usersPresent: true,
+    },
+  ],
+  postTags: [],
+  directions: [
+    {
+      id: 3,
+      name: 'surgery',
+      label: 'Хірургія',
+      color: '#7aebbf',
+      hasDoctors: true,
+      hasPosts: true,
+    },
+    {
+      id: 6,
+      name: 'cardiology',
+      label: 'Кардіологія',
+      color: '#00ffff',
+      hasDoctors: true,
+      hasPosts: true,
+    },
+    {
+      id: 5,
+      name: 'virology',
+      label: 'Вірусологія',
+      color: '#da80e8',
+      hasDoctors: true,
+      hasPosts: true,
+    },
+    {
+      id: 2,
+      name: 'ophthalmology',
+      label: 'Офтальмологія',
+      color: '#98ef50',
+      hasDoctors: true,
+      hasPosts: true,
+    },
+    {
+      id: 4,
+      name: 'therapy',
+      label: 'Терапія',
+      color: '#ffee58',
+      hasDoctors: true,
+      hasPosts: true,
+    },
+    {
+      id: 1,
+      name: 'covid-19',
+      label: 'Covid-19',
+      color: '#ef5350',
+      hasDoctors: true,
+      hasPosts: true,
+    },
+    {
+      id: 7,
+      name: 'pediatrics',
+      label: 'Педіатрія',
+      color: '#993333',
+      hasDoctors: true,
+      hasPosts: true,
+    },
+  ],
+  origins: [
+    {
+      id: 1,
+      name: 'Думка експерта',
+      parameters: null,
+    },
+    {
+      id: 2,
+      name: 'Медитека',
+      parameters: null,
+    },
+    {
+      id: 3,
+      name: 'Переклад',
+      parameters: null,
+    },
+  ],
+  statuses: [
+    {
+      id: 0,
+      name: 'Чернетка',
+    },
+    {
+      id: 1,
+      name: 'Не переглянутий',
+    },
+    {
+      id: 2,
+      name: 'На модерації',
+    },
+    {
+      id: 3,
+      name: 'Опубліковано',
+    },
+    {
+      id: 4,
+      name: 'Архівований',
+    },
+  ],
+  loading: 'succeeded',
+};
