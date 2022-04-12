57f34c0af3577aae60528837fb4e51acb57d390d
besizzo <besizzo@gmail.com>
2022-04-12 20:05:45 +0300

    help is coming!

--- src/components/Header/ArticleSearch.tsx
+++ src/components/Header/ArticleSearch.tsx
@@ -5,9 +5,7 @@ import InputAdornment from '@material-ui/core/InputAdornment';
 import ClickAwayListener from '@material-ui/core/ClickAwayListener';
 import Autocomplete from '@material-ui/lab/Autocomplete';
 import SearchIcon from '@material-ui/icons/Search';
-import CircularProgress from '@material-ui/core/CircularProgress';
 import { useActions } from '../../shared/hooks';
-
 import { useStyles } from './Header.styles';
 import {
   setField,
@@ -17,8 +15,8 @@ import {
 } from '../../models/adminLab';
 import { useEffectExceptOnMount } from '../../old/lib/hooks/useEffectExceptOnMount';
 
-import { IPostsOBJ } from '../../models/adminLab/types';
-import { IPost } from '../../old/lib/types';
+// import { IPostsOBJ } from '../../models/adminLab/types';
+// import { IPost } from '../../old/lib/types';
 
 interface IArticleSearch {
   setVisibility: React.Dispatch<React.SetStateAction<boolean>>;
@@ -34,22 +32,9 @@ export const ArticleSearch: React.FC<IArticleSearch> = ({ setVisibility }) => {
   const [open, setOpen] = useState<boolean>(false);
   const [value, setValue] = useState<string>('');
   const theValue = value;
-  const initialOptions = [
-    {
-      id: 1,
-      title: 'foo',
-    },
-    {
-      id: 2,
-      title: 'baz',
-    },
-    {
-      id: 3,
-      title: 'bar',
-    },
-  ];
-  const [options, setOptions] = useState<IOptions[]>(initialOptions);
-  const loading = open && options.length === 0;
+
+  const [options, setOptions] = useState<IOptions[]>([]);
+  // const loading = open && options.length === 0;
 
   const [boundedSetField, boundedGetMaterialsAction] = useActions([
     setField,
@@ -67,12 +52,7 @@ export const ArticleSearch: React.FC<IArticleSearch> = ({ setVisibility }) => {
       return { id, title };
     });
     setOptions(titles);
-    console.log();
   }, [meta]);
-
-  // setOptions(postIds.map((postId) => {
-  //   return { id: posts[postId].id, title: posts[postId].title };
-  // }));
 
   useEffect(() => {
     const timer = setTimeout(() => {
@@ -91,58 +71,46 @@ export const ArticleSearch: React.FC<IArticleSearch> = ({ setVisibility }) => {
     }
   }, [open]);
 
-  const topFilms = [
-    { title: 'The Shawshank Redemption', year: 1994 },
-    { title: 'The Godfather', year: 1972 },
-    { title: 'The Godfather: Part II', year: 1974 },
-    { title: 'The Dark Knight', year: 2008 },
-    { title: '12 Angry Men', year: 1957 },
-    { title: "Schindler's List", year: 1993 },
-    { title: 'Pulp Fiction', year: 1994 },
-  ];
-
-  function handleChange(
+  const handleChange = (
     event: React.ChangeEvent<HTMLTextAreaElement | HTMLInputElement>,
-  ) {
+  ) => {
     if (event.target.value !== '' || event.target.value !== null) {
       setValue(event.target.value);
     }
-  }
+  };
 
   return (
-    // <ClickAwayListener onClickAway={() => setVisibility(false)}>
-    <Autocomplete
-      // filterOptions={(x) => x}
-      fullWidth
-      open={open}
-      onOpen={() => {
-        setOpen(true);
-      }}
-      onClose={() => {
-        setOpen(false);
-      }}
-      loading={loading}
-      options={topFilms.map((option) => option.title)}
-      renderInput={(params) => (
-        <TextField
-          // eslint-disable-next-line react/jsx-props-no-spreading
-          {...params}
-          // fullWidth
-          value={theValue}
-          variant="standard"
-          className={classes.searchInput}
-          onChange={(event) => handleChange(event)}
-          InputProps={{
-            endAdornment: (
-              <InputAdornment position="end">
-                <SearchIcon className={classes.searchInputIcon} />
-              </InputAdornment>
-            ),
-            disableUnderline: true,
-          }}
-        />
-      )}
-    />
-    // </ClickAwayListener>
+    <ClickAwayListener onClickAway={() => setVisibility(false)}>
+      <Autocomplete
+        filterOptions={(x) => x}
+        fullWidth
+        open={open}
+        onOpen={() => {
+          setOpen(true);
+        }}
+        onClose={() => {
+          setOpen(false);
+        }}
+        options={options.map((option) => option.title)}
+        renderInput={(params) => (
+          <TextField
+            // eslint-disable-next-line react/jsx-props-no-spreading
+            {...params}
+            value={theValue}
+            variant="standard"
+            className={classes.searchInput}
+            onChange={(event) => handleChange(event)}
+            InputProps={{
+              endAdornment: (
+                <InputAdornment position="end">
+                  <SearchIcon className={classes.searchInputIcon} />
+                </InputAdornment>
+              ),
+              disableUnderline: true,
+            }}
+          />
+        )}
+      />
+    </ClickAwayListener>
   );
 };
