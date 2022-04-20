17bb0bb288a0f04fd23d465c9cc5d374da6bb3ef
Dergelyova <88082907+Dergelyova@users.noreply.github.com>
2022-04-20 17:10:59 +0300

    Fixed ESLint errors

--- src/views/Profile/AuthorsList/AuthorsList.tsx
+++ src/views/Profile/AuthorsList/AuthorsList.tsx
@@ -1,95 +1,13 @@
 import React from 'react';
 import { Paper, Table, TableContainer, Box, Button } from '@material-ui/core';
+import { IAdminLabExpert } from 'models/adminLab/types';
 import AuthorsTableHead from './AuthorsTableHead';
 import AuthorsTableBody from './AuthorsTableBody';
-import { IAdminLabExpert } from 'models/adminLab/types';
 import { useStyles } from './styles/AuthorsList.styles';
-
-// /*Temporary Mock for authors info until getting API*/
-
-// const authors: IAdminLabExpert[] = [
-//   {
-//     id: 10,
-//     firstName: 'Марія',
-//     lastName: 'Марієнко',
-//     region: 'Київська область',
-//     dateOfCreation: '12.03.2022',
-//     dateOfEdition: '12.03.2022',
-//     bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
-//     mainInstitution: {
-//       id: 5,
-//       city: {
-//         id: 55,
-//         name: 'Бровари',
-//       },
-//       name: 'Medical Idea',
-//     },
-//   },
-//   {
-//     id: 6,
-//     firstName: 'Палана',
-//     lastName: 'Литвинова',
-//     region: 'Дніпропетровська область',
-//     dateOfCreation: '12.03.2022',
-//     dateOfEdition: '12.03.2022',
-//     bio:
-//       'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ',
-//     mainInstitution: {
-//       id: 4,
-//       city: {
-//         id: 119,
-//         name: 'Дніпро',
-//       },
-//       name: 'Медікум',
-//     },
-//   },
-//   {
-//     id: 6,
-//     firstName: 'Таржеман',
-//     lastName: 'Соколов',
-//     region: 'Дніпропетровська область',
-//     dateOfCreation: '12.03.2022',
-//     dateOfEdition: '12.03.2022',
-//     bio:
-//       'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor ',
-
-//     mainInstitution: {
-//       id: 4,
-//       city: {
-//         id: 119,
-//         name: 'Дніпро',
-//       },
-//       name: 'Медікум',
-//     },
-//     lastAddedPost: {
-//       id: 246,
-//       title: 'Sit amet consectetur',
-//     },
-//   },
-//   {
-//     id: 16,
-//     firstName: 'Олег',
-//     lastName: 'Петренко',
-//     region: 'Київська область',
-//     dateOfCreation: '12.03.2022',
-//     dateOfEdition: '12.03.2022',
-//     bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
-//     mainInstitution: {
-//       id: 5,
-//       city: {
-//         id: 55,
-//         name: 'Бровари',
-//       },
-//       name: 'Medical Idea',
-//     },
-//   },
-// ];
-
-// /*End of Mock section */
 
 export const AuthorsList: React.FC = () => {
   const classes = useStyles();
-  /*Temporary Mock for authors info until getting API*/
+  /* Temporary Mock for authors info until getting API */
 
   const authors: IAdminLabExpert[] = [
     {
@@ -169,7 +87,7 @@ export const AuthorsList: React.FC = () => {
     },
   ];
 
-  /*End of Mock section */
+  /* End of Mock section */
 
   return (
     <>
