13a7efb248f90ff045c358a9b80fd138f12cacd1
Dergelyova <88082907+Dergelyova@users.noreply.github.com>
2022-06-29 20:23:57 +0300

    US#555: created components for authors table

---
+++ src/views/Profile/AuthorsList/AuthorsTablePagination.tsx
@@ -0,0 +1,7 @@
+import { Pagination } from '@material-ui/lab';
+import React from 'react';
+
+export const AuthorsTablePagination: React.FC = () => {
+  /* need to put logic to this component  */
+  return <Pagination count={10} />;
+};
