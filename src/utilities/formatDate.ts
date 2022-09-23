939f7bc18526c0b570c1748d1d0c2c37320f24ec
Aliona <anechyt@softserveinc.com>
2022-09-23 17:21:50 +0300

    bug 563: update date formating and related tests (#787)
    * bug 563: update date formating and related tests
    
    * bug 563: improve hh:mm processing

--- src/utilities/formatDate.ts
+++ src/utilities/formatDate.ts
@@ -1,22 +1,31 @@
 import i18n, { langTokens } from '../locales/localizationInit';
 
-export const formatDate = (dateString = ''): string => {
-  const monthNames: { [index: string]: string } = {
-    '1': i18n.t(langTokens.date.januaryGenitiveCase),
-    '2': i18n.t(langTokens.date.februaryGenitiveCase),
-    '3': i18n.t(langTokens.date.marchGenitiveCase),
-    '4': i18n.t(langTokens.date.aprilGenitiveCase),
-    '5': i18n.t(langTokens.date.mayGenitiveCase),
-    '6': i18n.t(langTokens.date.juneGenitiveCase),
-    '7': i18n.t(langTokens.date.julyGenitiveCase),
-    '8': i18n.t(langTokens.date.augustGenitiveCase),
-    '9': i18n.t(langTokens.date.septemberGenitiveCase),
-    '10': i18n.t(langTokens.date.octoberGenitiveCase),
-    '11': i18n.t(langTokens.date.novemberGenitiveCase),
-    '12': i18n.t(langTokens.date.decemberGenitiveCase),
-  };
+const monthNames: { [index: string]: string } = {
+  '1': i18n.t(langTokens.date.januaryGenitiveCase),
+  '2': i18n.t(langTokens.date.februaryGenitiveCase),
+  '3': i18n.t(langTokens.date.marchGenitiveCase),
+  '4': i18n.t(langTokens.date.aprilGenitiveCase),
+  '5': i18n.t(langTokens.date.mayGenitiveCase),
+  '6': i18n.t(langTokens.date.juneGenitiveCase),
+  '7': i18n.t(langTokens.date.julyGenitiveCase),
+  '8': i18n.t(langTokens.date.augustGenitiveCase),
+  '9': i18n.t(langTokens.date.septemberGenitiveCase),
+  '10': i18n.t(langTokens.date.octoberGenitiveCase),
+  '11': i18n.t(langTokens.date.novemberGenitiveCase),
+  '12': i18n.t(langTokens.date.decemberGenitiveCase),
+};
 
-  const [day, month] = dateString.length ? dateString.split('.') : [null, null];
-
-  return day && month ? `${+day.toString()} ${monthNames[+month]}` : '';
+export const formatDate = (dateString = '', time = false): string => {
+  if (dateString.length) {
+    const fullDate = new Date(dateString);
+    const hours = fullDate.getHours().toString().padStart(2, '0');
+    const mins = fullDate.getMinutes().toString().padStart(2, '0');
+    const displayDate = time
+      ? `${hours}:${mins}, ${fullDate.getDate()} ${
+          monthNames[fullDate.getMonth() + 1]
+        } ${fullDate.getFullYear()}`
+      : `${fullDate.getDate()} ${monthNames[fullDate.getMonth() + 1]}`;
+    return displayDate;
+  }
+  return dateString;
 };
