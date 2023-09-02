be6675499abf3821ce9d13ceb95dd3e5621b2be0
Oleh Filiak <oleh.filyak92@gmail.com>
2023-09-02 13:03:53 +0300

    feat(DatePicker): change styles for selectedDay
    Initially, the DatePicker style for SelectedDay is not displayed when you open it. As soon as it is selected, the background color is only for the selected element.

--- src/views/Profile/AdminTable/AdminDatePicker.tsx
+++ src/views/Profile/AdminTable/AdminDatePicker.tsx
@@ -1,13 +1,16 @@
-import React from 'react';
+import React, { useReducer } from 'react';
 import FormControl from '@material-ui/core/FormControl';
 import Select from '@material-ui/core/Select';
-import { parsDate } from 'utilities/parsDate';
-import { DatePicker } from '@material-ui/pickers';
-import { requestDate } from 'utilities/formatDate';
+import { parsDate } from 'utilities/parsDate'; // Import as needed
+import { DatePicker } from '@material-ui/pickers'; // Import as needed
+import { requestDate } from 'utilities/formatDate'; // Import as needed
 import { useTranslation } from 'react-i18next';
 import { Box } from '@material-ui/core';
 import { langTokens } from '../../../locales/localizationInit';
-import { useStyles } from './styles/AdminDatePicker.styles';
+import {
+  useStyles,
+  useStylesForDatePicker,
+} from './styles/AdminDatePicker.styles'; // Import as needed
 
 type VerticalType = number | 'bottom' | 'top' | 'center';
 type HorizontalType = number | 'center' | 'left' | 'right';
@@ -22,7 +25,6 @@ const MenuProps = {
     vertical: 'top' as VerticalType,
     horizontal: 'left' as HorizontalType,
   },
-
   getContentAnchorEl: null,
 };
 
@@ -32,18 +34,50 @@ interface IMaterialsDate {
   setChanges: (...params: any[]) => void;
 }
 
+const datePickerReducer = (
+  state: { start: string; end: string },
+  action: { type: string; position: string },
+) => {
+  switch (action.type) {
+    case 'SET_BG_COLOR':
+      return {
+        ...state,
+        [action.position]: 'grey',
+      };
+    default:
+      return state;
+  }
+};
+
 export const AdminDatePicker: React.FC<IMaterialsDate> = ({
   start,
   end,
   setChanges,
 }) => {
+  const [datePickerState, dispatch] = useReducer(datePickerReducer, {
+    start: 'transparent',
+    end: 'transparent',
+  });
+
   const classes = useStyles();
+  const datepickerClassesStart = useStylesForDatePicker({
+    bgColorForDatePicker: datePickerState.start,
+  });
+  const datepickerClassesEnd = useStylesForDatePicker({
+    bgColorForDatePicker: datePickerState.end,
+  });
+
   const { t } = useTranslation();
-  const handleDatePicker = (option: string, value) => {
-    setChanges({
-      option,
-      date: requestDate(value),
-    });
+  const today = new Date();
+
+  const handleDatePicker = (option: string, value: Date | null) => {
+    if (value) {
+      dispatch({ type: 'SET_BG_COLOR', position: option });
+      setChanges({
+        option,
+        date: requestDate(value),
+      });
+    }
   };
 
   return (
@@ -63,30 +97,35 @@ export const AdminDatePicker: React.FC<IMaterialsDate> = ({
           MenuProps={MenuProps}
         >
           <Box display="flex" className={classes.pickersWrapper}>
-            <DatePicker
-              autoOk
-              orientation="portrait"
-              variant="static"
-              openTo="date"
-              value={start}
-              onChange={(value) => {
-                handleDatePicker('start', value);
-              }}
-              format="dd-mm-yyyy"
-              maxDate={end}
-            />
-            <DatePicker
-              autoOk
-              orientation="portrait"
-              variant="static"
-              openTo="date"
-              value={end}
-              onChange={(value) => {
-                handleDatePicker('end', value);
-              }}
-              format="dd-mm-yyyy"
-              minDate={start}
-            />
+            <Box className={datepickerClassesStart.datePicker}>
+              <DatePicker
+                autoOk
+                orientation="portrait"
+                variant="static"
+                openTo="date"
+                value={start}
+                onChange={(value) => {
+                  handleDatePicker('start', value);
+                }}
+                format="dd-mm-yyyy"
+                maxDate={today}
+              />
+            </Box>
+            <Box className={datepickerClassesEnd.datePicker}>
+              <DatePicker
+                autoOk
+                orientation="portrait"
+                variant="static"
+                openTo="date"
+                value={end}
+                onChange={(value) => {
+                  handleDatePicker('end', value);
+                }}
+                format="dd-mm-yyyy"
+                minDate={start}
+                maxDate={today}
+              />
+            </Box>
           </Box>
         </Select>
       </FormControl>
